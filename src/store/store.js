a46d8f03940f372416934a9c03e9ff18d0e215b2
Nikimad <PybePoiD@gmail.com>
2023-03-20 13:52:20 +0300

    Change /src/store/store: add observer reducer

--- src/store/store.js
+++ src/store/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import boardsAndTasks from "../models/boardsAndTasks/boardsAndTasksSlice";
 import boards from "../models/boards/boardsSlice";
 import tasks from "../models/tasks/tasksSlice";
+import observer from "../models/observer/observerSlice";
 
 export const store = configureStore({
   reducer: {
     boards,
     tasks,
     boardsAndTasks,
+    observer,
   },
 });
