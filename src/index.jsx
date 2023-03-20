bb52e68620831dddd0d4a830da6d1f268e068a88
Nikimad <PybePoiD@gmail.com>
2023-03-20 11:36:41 +0300

    Change /src/index.jsx: add store, global style, home page

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
+import "./styles/global.scss";
+import HomePage from "./pages/HomePage";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider />
-  </React.StrictMode>,
+    <Provider store={store}>
+      <HomePage />
+    </Provider>
+  </React.StrictMode>
 );
