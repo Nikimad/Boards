da39139f2b80e3c0f5295e564193394d8d19537c
Nikimad <PybePoiD@gmail.com>
2023-03-21 14:12:26 +0300

    Fix /src/helpers/validate.js: no validators case

--- src/helpers/validate.js
+++ src/helpers/validate.js
@@ -1,12 +1,12 @@
-const validate = (state) => {
-  const errors = Object.entries(state.validators).reduce(
+const validate = (fields, validators = {}) => {
+  const errors = Object.entries(validators).reduce(
     (acc, [fieldName, validators]) => {
       let message;
 
       for (let i = 0; i < validators.length; i += 1) {
         const validator = validators[i];
 
-        message = validator(fieldName, state.fields[fieldName]) ?? null;
+        message = validator(fieldName, fields[fieldName]) ?? null;
 
         if (Boolean(message)) break;
       }
