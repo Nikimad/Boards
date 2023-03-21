eebf4677b5e8a6abca48e36e6ebc7e8ca5fd293b
Nikimad <PybePoiD@gmail.com>
2023-03-21 13:56:29 +0300

    Refact /src/hooks/useForm.js

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,17 +1,26 @@
-import { useCallback, useReducer } from "react";
+import { useCallback, useEffect, useReducer } from "react";
+import getInitialState from "../helpers/getInitialFormState";
+import getId from "../helpers/getId";
+import validate from "../helpers/validate";
+import useAction from "./useAction";
 
-const useForm = (initialState) => {
+const useForm = (externalState, submitAction) => {
+  const initialState = getInitialState(externalState);
+
   const ACTIONS = {
     change: "CHANGE",
     reset: "RESET",
+    validate: "VALIDATE",
   };
 
   const reducer = (state, { type, payload }) => {
     switch (type) {
       case ACTIONS.change:
-        return { ...state, ...payload };
+        return { ...state, fields: { ...state.fields, ...payload } };
       case ACTIONS.reset:
         return { ...initialState };
+      case ACTIONS.validate:
+        return { ...state, ...validate(state) };
       default:
         return state;
     }
@@ -19,29 +28,46 @@ const useForm = (initialState) => {
 
   const [state, dispatch] = useReducer(reducer, initialState);
 
-  const changeAction = useCallback(
-    ({ name, value }) => ({
-      type: ACTIONS.change,
-      payload: { [name]: value },
-    }),
-    [ACTIONS.change]
-  );
-  const resetAction = useCallback(
-    () => ({ type: ACTIONS.reset }),
-    [ACTIONS.reset]
+  const changeAction = useCallback(({ name, value }) => ({
+    type: ACTIONS.change,
+    payload: { [name]: value },
+  }), [ACTIONS.change]);
+
+  const resetAction = useCallback(() => ({ type: ACTIONS.reset }), [ACTIONS.reset]);
+  const validateAction = useCallback(() => ({ type: ACTIONS.validate }), [ACTIONS.validate]);
+
+  const resetForm = useCallback(() => dispatch(resetAction()), [resetAction]);
+  const validateForm = useCallback(() => dispatch(validateAction()), [validateAction]);
+
+  const dispatchSubmitAction = useAction(
+    submitAction({
+      id: getId(),
+      ...state.fields,
+    })
   );
 
-  const changeFieldValue = useCallback(
+  useEffect(() => {
+    if (state.isValid) {
+      dispatchSubmitAction();
+      resetForm();
+    }
+  }, [state.isValid, dispatchSubmitAction, resetForm]);
+
+
+  const change = useCallback(
     ({ target }) => dispatch(changeAction(target)),
     [changeAction]
   );
-
-  const resetForm = useCallback(
-    () => dispatch(resetAction()),
-    [resetAction]
+  
+  const submit = useCallback(
+    (e) => {
+      e.preventDefault();
+      validateForm();
+    },
+    [validateForm]
   );
 
-  return [state, changeFieldValue, resetForm];
+  return [state, change, submit];
 };
 
 export default useForm;
