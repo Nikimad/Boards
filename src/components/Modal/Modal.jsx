3f7b34086d7632fc1a6a8d2b8d9d80bc0ed14bfe
Nikimad <PybePoiD@gmail.com>
2023-03-22 11:26:21 +0300

    Add /src/components: Modal component

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,18 @@
+import { createPortal } from "react-dom";
+import "./Modal.scss";
+
+const modalEl = document.getElementById("modal");
+
+const Modal = ({ status, onReset, onContentClick, children }) =>
+  createPortal(
+    status === "close" ? null : (
+      <div className="modal" data-status={status} onClick={onReset}>
+        <div className="modal__content" onClick={onContentClick}>
+          {children}
+        </div>
+      </div>
+    ),
+    modalEl
+  );
+
+export default Modal;
---
+++ src/components/Modal/ModalContainer.jsx
@@ -0,0 +1,38 @@
+import { useCallback, useEffect } from "react";
+import Modal from "./Modal";
+
+const ModalContainer = ({ status, onReset, onClose, children }) => {
+  const handleEscape = useCallback(
+    (e) => {
+      if (e.key === "Escape" && status === "show") onReset();
+    },
+    [onReset]
+  );
+  
+  const handleContentClick = (e) => e.stopPropagation();
+
+  useEffect(() => {
+    let timeout;
+    if (status === "reset") {
+      timeout = setTimeout(onClose, 200);
+    }
+
+    document.addEventListener("keydown", handleEscape);
+
+    return () => {
+      clearTimeout(timeout);
+      document.removeEventListener("keydown", handleEscape);
+    };
+  }, [onClose, handleEscape]);
+
+  return (
+    <Modal
+      status={status}
+      onReset={onReset}
+      onContentClick={handleContentClick}
+      children={children}
+    />
+  );
+};
+
+export default ModalContainer;
