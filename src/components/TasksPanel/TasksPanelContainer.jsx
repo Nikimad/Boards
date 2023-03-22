440af585d26c9fd0f76fc998398aa952650f5fe9
Nikimad <PybePoiD@gmail.com>
2023-03-22 14:11:25 +0300

    Add /src/components: TaskPanel component

---
+++ src/components/TasksPanel/TasksPanelContainer.jsx
@@ -0,0 +1,12 @@
+import { useSelector } from "react-redux";
+import {selectCurrentBoardId, selectCurrentTasks } from "../../models/observer/observerSelectors";
+import TasksPanel from "./TasksPanel";
+
+const TasksPanelContainer = () => {
+  const tasksIds = useSelector(selectCurrentTasks);
+  const currentBoardId = useSelector(selectCurrentBoardId);
+
+  return <TasksPanel tasksIds={tasksIds} isBoardChoosen={Boolean(currentBoardId)} />;
+};
+
+export default TasksPanelContainer;
