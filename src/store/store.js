d09a901f8629c96242c16af6e89972a40e51a883
Nikimad <PybePoiD@gmail.com>
2023-03-22 15:30:48 +0300

    Change /src/store/store: add local storage

--- src/store/store.js
+++ src/store/store.js
@@ -2,6 +2,10 @@ import { configureStore } from "@reduxjs/toolkit";
 import boards from "../models/boards/boardsSlice";
 import tasks from "../models/tasks/tasksSlice";
 import observer from "../models/observer/observerSlice";
+import { loadState, saveState } from "../helpers/localStorageHandlers";
+import  throttle  from "lodash/throttle";
+
+const state = loadState();
 
 export const store = configureStore({
   reducer: {
@@ -9,4 +13,7 @@ export const store = configureStore({
     tasks,
     observer,
   },
+  preloadedState: state,
 });
+
+store.subscribe(throttle(() => saveState(store.getState()), 1000, { leading: false }));
