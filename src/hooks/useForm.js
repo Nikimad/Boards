e17b637901b225ca0db61cb44844d10d580bfc50
Nikimad <PybePoiD@gmail.com>
2023-03-22 11:23:55 +0300

    Change /src/hooks/useForm: remove form control

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,10 +1,8 @@
-import { useCallback, useEffect, useReducer } from "react";
+import { useCallback, useReducer } from "react";
 import getInitialState from "../helpers/getInitialFormState";
-import getId from "../helpers/getId";
 import validate from "../helpers/validate";
-import useAction from "./useAction";
 
-const useForm = (externalState, submitAction) => {
+const useForm = (externalState) => {
   const initialState = getInitialState(externalState);
 
   const ACTIONS = {
@@ -32,42 +30,17 @@ const useForm = (externalState, submitAction) => {
     type: ACTIONS.change,
     payload: { [name]: value },
   }), [ACTIONS.change]);
-
   const resetAction = useCallback(() => ({ type: ACTIONS.reset }), [ACTIONS.reset]);
   const validateAction = useCallback(() => ({ type: ACTIONS.validate }), [ACTIONS.validate]);
 
-  const resetForm = useCallback(() => dispatch(resetAction()), [resetAction]);
-  const validateForm = useCallback(() => dispatch(validateAction()), [validateAction]);
-
-  const dispatchSubmitAction = useAction(
-    submitAction({
-      id: getId(),
-      ...state.fields,
-    })
-  );
-
-  useEffect(() => {
-    if (state.isValid) {
-      dispatchSubmitAction();
-      resetForm();
-    }
-  }, [state.isValid, dispatchSubmitAction, resetForm]);
-
-
-  const change = useCallback(
+  const changeForm = useCallback(
     ({ target }) => dispatch(changeAction(target)),
     [changeAction]
   );
-  
-  const submit = useCallback(
-    (e) => {
-      e.preventDefault();
-      validateForm();
-    },
-    [validateForm]
-  );
+  const resetForm = useCallback(() => dispatch(resetAction()), [resetAction]);
+  const validateForm = useCallback(() => dispatch(validateAction()), [validateAction]);
 
-  return [state, change, submit];
+  return [state, changeForm, resetForm, validateForm];
 };
 
 export default useForm;
