c0cb50dd410439e23a3ee61a0b5ca3cbf106fdd1
Nikimad <PybePoiD@gmail.com>
2023-03-22 14:14:00 +0300

    Change /src/pages/HomePage: add Sidebar, Header, TasksPanel els

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,15 @@
 import Wrapper from "../../components/Wrapper";
+import Sidebar from "../../components/Sidebar";
+import Header from "../../components/Header";
+import TasksPanel from "../../components/TasksPanel";
+
 
 const HomePage = () => (
-  <Wrapper />
+  <Wrapper>
+    <Sidebar />
+    <Header />
+    <TasksPanel />
+  </Wrapper>
 );
 
 export default HomePage;
