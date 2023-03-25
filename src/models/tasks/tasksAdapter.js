babc310b780e8430e9a8e6f0f29646dbca0d46b6
Nikimad <PybePoiD@gmail.com>
2023-03-25 09:29:48 +0300

    Remove /src/models/*/*Adapter

--- src/models/tasks/tasksAdapter.js
+++
@@ -1,6 +0,0 @@
-import { createEntityAdapter } from "@reduxjs/toolkit";
-
-export const tasksAdapter = createEntityAdapter({
-  selectId: (task) => task.id,
-  sortComparer: (a, b) => b.id - a.id,
-});
