43058cecbeaa6afc4c745a46ab837cf67ca80b8f
Nikimad <PybePoiD@gmail.com>
2023-03-25 09:35:18 +0300

    Add /src/helpers: getTaskFormProps.jsx

---
+++ src/helpers/getTaskFormProps.js
@@ -0,0 +1,17 @@
+import * as Yup from "yup";
+
+const getTaskFormProps = (
+  initialValues = {
+    title: "",
+  }
+) => ({
+  initialValues,
+  validationSchema: Yup.object({
+    title: Yup.string()
+      .min(5, "Title must be 5 characters or more")
+      .max(10, "Title must be 10 characters or less")
+      .required("Title is required"),
+  }),
+});
+
+export default getTaskFormProps;
