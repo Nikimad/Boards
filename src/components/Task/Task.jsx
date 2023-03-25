bf54c87124fa892a93a739f6ab2ba44085efa72d
Nikimad <PybePoiD@gmail.com>
2023-03-25 09:25:43 +0300

    Refactor /src/components/Task

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,22 +1,47 @@
 import Modal from "../Modal";
 import "./Task.scss";
+import { ReactComponent as Edit } from "../../assets/svg/edit.svg";
+import TaskForm from "../TaskForm";
 
 const Task = ({
-  title,
-  id,
+  taskValues,
   showModal,
-  modalStatus,
-  resetModal,
-  closeModal,
+  modalProps,
+  isEdit,
+  onEdit,
+  onSubmit,
+  onRemove,
 }) => (
   <li className="task" onDoubleClick={showModal}>
-    <h3 className="task__title">{title}</h3>
-    <p className="task__temp">{new Date(id).toTimeString()}</p>
-    <Modal status={modalStatus} onReset={resetModal} onClose={closeModal}>
-      <div className="task task__review">
-        <h3 className="task__title">{title}</h3>
-        <p className="task__temp">{new Date(id).toTimeString()}</p>
-      </div>
+    <h3 className="task__title">{taskValues.title}</h3>
+    <Modal {...modalProps}>
+      {isEdit ? (
+        <TaskForm
+          formTitle="Edit Task"
+          initialValues={taskValues}
+          onSubmit={onSubmit}
+        >
+          <fieldset>
+            <label>
+              <input className="form__submit" type="submit" value="Save" />
+            </label>
+            <label>
+              <input
+                className="form__submit"
+                type="button"
+                value="Remove"
+                onClick={onRemove}
+              />
+            </label>
+          </fieldset>
+        </TaskForm>
+      ) : (
+        <div className="task task__review">
+          <h3 className="task__title">{taskValues.title}</h3>
+          <Edit onClick={onEdit} width={"2em"} />
+          <button onClick={onRemove}>Remove</button>
+        </div>
+      )}
     </Modal>
   </li>
 );
--- src/components/Task/TaskContainer.jsx
+++ src/components/Task/TaskContainer.jsx
@@ -1,13 +1,45 @@
-import { useSelector } from "react-redux";
-import { tasksSelectors } from "../../models/tasks/tasksSelectors";
 import useModal from "../../hooks/useModal";
 import Task from "./Task";
+import { useState } from "react";
+import useStateDepAction from "../../hooks/useStateDepAction";
+import { updateTask, removeTask } from "../../models/tasks/tasksSlice";
+import useAction from "../../hooks/useAction";
 
-const TaskContainer = ({ id }) => {
-  const task = useSelector((state) => tasksSelectors.selectById(state, id));
-  const modalProps = useModal();
+const TaskContainer = ({ task }) => {
+  const { id, ...taskValues } = task;
+  const { showModal, resetModal, ...modalProps } = useModal();
+  const [isEdit, setIsEdit] = useState(false);
+  const handleEditClick = () => setIsEdit(true);
 
-  return <Task {...task} {...modalProps} />;
+  const dispatchStateDepUpdateTask = useStateDepAction(updateTask);
+  const dispatchRemoveTask = useAction(removeTask(id));
+
+  const resetTask = () => {
+    resetModal();
+    setIsEdit(false);
+  };
+
+  const handleUpdateBoard = (values) => {
+    dispatchStateDepUpdateTask({ id, changes: values });
+    setIsEdit(false);
+  };
+
+  const handleRemoveTask = () => {
+    dispatchRemoveTask();
+    removeTask();
+  };
+
+  return (
+    <Task
+      taskValues={taskValues}
+      isEdit={isEdit}
+      showModal={showModal}
+      modalProps={{ ...modalProps, resetModal: resetTask }}
+      onEdit={handleEditClick}
+      onSubmit={handleUpdateBoard}
+      onRemove={handleRemoveTask}
+    />
+  );
 };
 
 export default TaskContainer;
