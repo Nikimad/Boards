ce0633d51b7e6a904d1cc8d0908c56786a399546
Nikimad <PybePoiD@gmail.com>
2023-03-29 14:04:10 +0300

    Add /src/helpers: getBoardFormProps.js

--- src/helpers/getBoardFormProps.js
+++ src/helpers/getBoardFormProps.js
@@ -9,7 +9,7 @@ const getBoardFormProps = (
   validationSchema: Yup.object({
     title: Yup.string()
       .min(5, "Title must be 5 characters or more")
-      .max(10, "Title must be 10 characters or less")
+      .max(16, "Title must be 16 characters or less")
       .required("Title is required"),
   }),
 });
