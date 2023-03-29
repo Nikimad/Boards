17ba5ad34b533acc55b16821bd94d72a8cecd620
Nikimad <PybePoiD@gmail.com>
2023-03-29 14:32:36 +0300

    Fix /src/components/CurrentBoaard: remove board with tasks

--- src/components/CurrentBoard/CurrentBoardContainer.jsx
+++ src/components/CurrentBoard/CurrentBoardContainer.jsx
@@ -3,13 +3,16 @@ import useModal from "../../hooks/useModal";
 import useStateDepAction from "../../hooks/useStateDepAction";
 import useAction from "../../hooks/useAction";
 import { updateBoard, removeBoard } from "../../models/boards/boardsSlice";
+import { selectCurrentTasksIds } from "../../models/observer/observerSelectors";
+import { useSelector } from "react-redux";
 
 const CurrentBoardContainer = ({ board, toggleNav }) => {
   const { id, ...boardsValues } = board;
   const { showModal, ...modalProps } = useModal();
+  const currentTasksIds = useSelector(selectCurrentTasksIds);
 
   const dispatchStateDepUpdateBoard = useStateDepAction(updateBoard);
-  const dispatchRemoveBoard = useAction(removeBoard(id));
+  const dispatchRemoveBoard = useAction(removeBoard({ id, tasks: currentTasksIds}));
 
   const handleUpdateBoard = (values) => {
     dispatchStateDepUpdateBoard({ id, changes: values });
