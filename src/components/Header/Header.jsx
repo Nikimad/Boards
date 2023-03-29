96705b582aa1622658a86987575acb5e672ff46d
Nikimad <PybePoiD@gmail.com>
2023-03-29 13:47:30 +0300

    Refactor /src/components/Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,25 +1,18 @@
 import "./Header.scss";
-import TaskForm from "../TaskForm";
-import Modal from "../Modal";
+import CurrentBoard from "../CurrentBoard";
+import AddTask from "../AddTask";
+import LogoToggler from "../LogoToggler";
 
-const Header = ({ showModal, modalProps, currentBoard, onSubmit }) => (
-  <header className="header">
-    <h2 className="header__title">{currentBoard || "Choose board"}</h2>
-    <button
-      className="header__button"
-      onClick={showModal}
-      disabled={!Boolean(currentBoard)}
-    >
-      +<span className="header__button__text"> Add New Task</span>
-    </button>
-    <Modal {...modalProps}>
-      <TaskForm formTitle="Add New Task" onSubmit={onSubmit}>
-        <label>
-          <input className="form__submit" type="submit" value="Add New Task" />
-        </label>
-      </TaskForm>
-    </Modal>
-  </header>
+const Header = ({ currentBoard, toggleNav }) => (
+  <div className="header__wrapper">
+    <header className="header" data-empty={!Boolean(currentBoard)}>
+      <LogoToggler onClick={toggleNav} />
+      {Boolean(currentBoard) ? (
+        <CurrentBoard toggleNav={toggleNav} board={currentBoard} />
+      ) : null}
+      <AddTask />
+    </header>
+  </div>
 );
 
 export default Header;
--- src/components/Header/HeaderContainer.jsx
+++ src/components/Header/HeaderContainer.jsx
@@ -1,27 +1,14 @@
 import Header from "./Header";
-import useModal from "../../hooks/useModal";
 import { useSelector } from "react-redux";
 import { selectCurrentBoard } from "../../models/boards/boardsSelectors";
-import getId from "../../helpers/getId";
-import useStateDepAction from "../../hooks/useStateDepAction";
-import { addTask } from "../../models/tasks/tasksSlice";
 
-const HeaderContainer = () => {
-  const { showModal, ...modalProps } = useModal();
-  const dispatchStateDepAddTask = useStateDepAction(addTask);
+const HeaderContainer = (props) => {
   const currentBoard = useSelector(selectCurrentBoard);
-
-  const handleAddTask = (values) => {
-    dispatchStateDepAddTask({ id: getId(), ...values});
-    modalProps.resetModal();
-  };
 
   return (
     <Header
-      showModal={showModal}
-      modalProps={modalProps}
-      onSubmit={handleAddTask}
-      currentBoard={currentBoard?.title}
+      { ...props }
+      currentBoard={currentBoard}
     />
   );
 };
