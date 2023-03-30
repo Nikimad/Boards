770d48a12aab2f319a03f95c4a101f6356ea5735
Nikimad <PybePoiD@gmail.com>
2023-03-30 12:33:10 +0300

    Change /src/models/tasks/*Selectors: add selectStatusDepCurrentTasks

--- src/models/tasks/tasksSelectors.js
+++ src/models/tasks/tasksSelectors.js
@@ -1,18 +1,20 @@
-import { createSelector } from "@reduxjs/toolkit";
+import { createSelector, current } from "@reduxjs/toolkit";
 import { selectCurrentTasksIds } from "../observer/observerSelectors";
 
 const rootSelector = createSelector(
   (state) => state,
-  (state) => state.tasks,
+  (state) => state.tasks
 );
 
-export const selectTasks = createSelector(
-  rootSelector,
-  (tasks) => tasks.items,
-);
+export const selectTasks = createSelector(rootSelector, (tasks) => tasks.items);
 
 export const selectCurrentTasks = createSelector(
   selectCurrentTasksIds,
   selectTasks,
-  (ids, tasks) => tasks.filter((task) => ids.includes(task.id)),
+  (ids, tasks) => tasks.filter((task) => ids.includes(task.id))
 );
+
+export const selectStatusDepCurrentTasks = (status) =>
+  createSelector(selectCurrentTasks, (currentTasks) =>
+    currentTasks.filter((task) => task.status === status)
+  );
