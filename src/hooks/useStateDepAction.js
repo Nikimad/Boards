8bf081ea26ff644d2a359c602abb5d465433a9b2
Nikimad <PybePoiD@gmail.com>
2023-04-06 14:12:44 +0300

    Remove /src/hooks: useForm, useFormModal, useStateDepAction

--- src/hooks/useStateDepAction.js
+++
@@ -1,8 +0,0 @@
-import { useDispatch } from "react-redux";
-
-const useStateDepAction = (action) => {
-  const dispatch = useDispatch();
-  return (state) => dispatch(action(state));
-};
-
-export default useStateDepAction;
