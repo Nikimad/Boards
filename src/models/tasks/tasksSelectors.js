0aaf7f85b731d2c63ef116e530c405aeb10a4ddd
Nikimad <PybePoiD@gmail.com>
2023-04-06 14:07:51 +0300

    Refactor /src/models/tasks

--- src/models/tasks/tasksSelectors.js
+++ src/models/tasks/tasksSelectors.js
@@ -1,20 +1,37 @@
-import { createSelector, current } from "@reduxjs/toolkit";
-import { selectCurrentTasksIds } from "../observer/observerSelectors";
+import { createSelector } from "@reduxjs/toolkit";
+import { selectActiveBoardId } from "../boards/boardsSelectors";
 
 const rootSelector = createSelector(
   (state) => state,
   (state) => state.tasks
 );
 
-export const selectTasks = createSelector(rootSelector, (tasks) => tasks.items);
-
-export const selectCurrentTasks = createSelector(
-  selectCurrentTasksIds,
-  selectTasks,
-  (ids, tasks) => tasks.filter((task) => ids.includes(task.id))
+export const selectActiveTaskId = createSelector(
+  rootSelector,
+  (tasks) => tasks.activeTaskId
 );
 
-export const selectStatusDepCurrentTasks = (status) =>
-  createSelector(selectCurrentTasks, (currentTasks) =>
-    currentTasks.filter((task) => task.status === status)
-  );
+export const selectTasks = createSelector(rootSelector, (tasks) => tasks.tasks);
+
+export const selectTasksId = createSelector(
+  rootSelector,
+  (tasks) => tasks.tasksIds
+);
+
+export const selectTasksArr = createSelector(
+  selectTasks,
+  selectTasksId,
+  selectActiveBoardId,
+  (tasks, ids, boardId) => ids.reduce((acc, id) => {
+    if (tasks[id].boardId === boardId) {
+      acc = [...acc, tasks[id]];
+    }
+    return acc;
+  }, [])
+);
+
+export const selectActiveTask = createSelector(
+  selectTasks,
+  selectActiveTaskId,
+  (tasks, id) => tasks[id]
+);
--- src/models/tasks/tasksSlice.js
+++ src/models/tasks/tasksSlice.js
@@ -1,31 +1,55 @@
 import { createSlice } from "@reduxjs/toolkit";
+import getId from "../../helpers/getId";
 import { removeBoard } from "../boards/boardsSlice";
 
 const tasksSlice = createSlice({
   name: "tasks",
   initialState: {
-    items: [],
+    activeTaskId: null,
+    tasks: {},
+    tasksIds: [],
   },
   reducers: {
-    addTask(state, {payload}) {
-      state.items = [ payload, ...state.items]
+    setActiveTaskId(state, { payload }) {
+      state.activeTaskId = payload;
+    },
+    addTask(state, { payload }) {
+      const id = getId();
+      state.tasks[id] = { id, ...payload };
+      state.tasksIds = [id, ...state.tasksIds];
+    },
+    editTask(state, { payload }) {
+      const { id, ...values } = payload;
+      state.tasks[id] = {
+        ...state.tasks[id],
+        ...values,
+      };
     },
     removeTask(state, { payload }) {
-      state.items = state.items.filter((board) => board.id !== payload);
-    },
-    updateTask(state, { payload }) {
-      state.items = state.items.map((board) =>
-        board.id === payload.id ? { ...board, ...payload.changes } : board
-      );
+      state.activeTaskId = null;
+      delete state.tasks[payload];
+      state.tasksIds = state.tasksIds.filter((id) => id !== payload);
     },
   },
   extraReducers(builder) {
     builder.addCase(removeBoard, (state, { payload }) => {
-      state.items = state.items.filter((task) => !payload.tasks.includes(task.id));
+      const deletedTasksId = Object.entries(state.tasks).filter(
+        ([id, task]) => {
+          if (task.boardId === payload) return id;
+        }
+      );
+      state.tasksIds = state.tasksIds.filter(
+        (id) => !deletedTasksId.includes(id)
+      );
+      state.tasks = state.tasksIds.reduce((tasks, id) => {
+        tasks[id] = state.tasks[id];
+        return tasks;
+      }, {});
     });
   },
 });
 
-export const { addTask, removeTask, updateTask } = tasksSlice.actions;
+export const { setActiveTaskId, addTask, editTask, removeTask } =
+  tasksSlice.actions;
 
 export default tasksSlice.reducer;
