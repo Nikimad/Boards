8d3278a7d69abb750817617f442ab6274abd6afd
Nikimad <PybePoiD@gmail.com>
2023-04-18 15:37:12 +0300

    Refactor /src/components/TaskPreview

--- src/components/TaskPreview/TaskPreview.jsx
+++ src/components/TaskPreview/TaskPreview.jsx
@@ -1,16 +1,15 @@
 import "./TaskPreview.scss";
-import { Form, Field } from "formik";
 import { ReactComponent as View } from "../../assets/svg/view.svg";
-import { ReactComponent as Select } from "../../assets/svg/select.svg";
+import TaskInteractionForm from "../TaskInteractionForm";
 import Modal from "../Modal";
 
 const TaskPreview = ({
+  id,
   title,
   status,
   subtasks,
   checkedSubtasks,
   setActiveTask,
-  submitOnChange,
 }) => (
   <div className="task-preview">
     <button onClick={setActiveTask} className="task-preview__content">
@@ -27,46 +26,7 @@ const TaskPreview = ({
       </button>
       <div className="task-review">
         <h2 className="task-review__title">{title}</h2>
-        <Form className="form task-review__form">
-          {subtasks.length > 0 ? (
-            <div
-              className="form__label"
-              role="group"
-              aria-labelledby="checkbox-group"
-            >
-              <span className="form__label__title">Subtasks</span>
-              {subtasks.map((subtask, index) => (
-                <label className="form__check" key={index}>
-                  <Field
-                    className="form__check__input"
-                    onChange={submitOnChange}
-                    type="checkbox"
-                    name="checkedSubtasks"
-                    value={`${subtask.id}`}
-                  />
-                  <span className="form__check__box"></span>
-                  {subtask.title}
-                </label>
-              ))}
-            </div>
-          ) : null}
-          <label className="form__label form__select">
-            <span className="form__label__title">Status</span>
-            <Field
-              className="form__input form__input_select"
-              onChange={submitOnChange}
-              name="status"
-              component="select"
-            >
-              <option defaultValue="todo" value="todo">
-                Todo
-              </option>
-              <option value="doing">Doing</option>
-              <option value="done">Done</option>
-            </Field>
-            <Select className="form__select-appearance" />
-          </label>
-        </Form>
+        <TaskInteractionForm {...{ id, status, subtasks, checkedSubtasks }} />
       </div>
     </Modal>
   </div>
--- src/components/TaskPreview/TaskPreviewContainer.jsx
+++ src/components/TaskPreview/TaskPreviewContainer.jsx
@@ -1,42 +1,18 @@
 import TaskPreview from "./TaskPreview";
-import { Formik } from "formik";
 import useAction from "../../hooks/useAction";
-import { setActiveTaskId, editTask } from "../../models/tasks/tasksSlice";
+import { setActiveTaskId } from "../../models/tasks/tasksSlice";
 
-const TaskPreviewContainer = ({ id, checkedSubtasks, status, ...props }) => {
+const TaskPreviewContainer = ({ task }) => {
   const dispatchSetActiveTaskId = useAction(setActiveTaskId);
-  const dispatchEditTask = useAction(editTask);
-
-  const setActiveTask = () => dispatchSetActiveTaskId(id);
-  const handleSubmit = (values) => dispatchEditTask({ id, ...values });
+  const setActiveTask = () => dispatchSetActiveTaskId(task.id);
 
   return (
-    <Formik
-      initialValues={{
-        checkedSubtasks,
-        status,
+    <TaskPreview
+      {...{
+        ...task,
+        setActiveTask,
       }}
-      onSubmit={handleSubmit}
-    >
-      {({ handleChange, submitForm }) => {
-        const submitOnChange = (e) => {
-          handleChange(e);
-          submitForm();
-        };
-
-        return (
-          <TaskPreview
-            {...{
-              ...props,
-              status,
-              checkedSubtasks,
-              setActiveTask,
-              submitOnChange,
-            }}
-          />
-        );
-      }}
-    </Formik>
+    />
   );
 };
 
