723dfce958e411f22c559bb9962ba4c3bcf3e429
Nikimad <PybePoiD@gmail.com>
2023-04-18 15:36:23 +0300

    Refactor /src/components/TaskForm

--- src/components/TaskForm/TaskForm.jsx
+++ src/components/TaskForm/TaskForm.jsx
@@ -1,9 +1,12 @@
-import { Form, Field, ErrorMessage } from "formik";
-import FormControls from "../FormControls";
+import "./TaskForm.scss";
+import { Field, FieldArray, ErrorMessage } from "formik";
+import FormWrapper from "../FormWrapper";
+import { ReactComponent as Delete } from "../../assets/svg/delete.svg";
+import { ReactComponent as Select } from "../../assets/svg/select.svg";
+import getId from "../../helpers/getId";
 
-const TaskForm = ({ formTitle, onRemove }) => (
-  <Form className="form">
-    <h2 className="form__title">{formTitle}</h2>
+const TaskForm = ({ values, ...props }) => (
+  <FormWrapper {...props}>
     <label className="form__label">
       <span className="form__label__title">Title</span>
       <Field className="form__input" name="title" type="text" />
@@ -13,21 +16,69 @@ const TaskForm = ({ formTitle, onRemove }) => (
     </label>
     <label className="form__label">
       <span className="form__label__title">Description</span>
-      <Field className="form__input form__textarea" name="description" component="textarea" />
+      <Field
+        className="form__input form__textarea"
+        name="description"
+        component="textarea"
+      />
       <ErrorMessage name="description">
         {(msg) => <span className="form__error">{msg}</span>}
       </ErrorMessage>
     </label>
-    <label className="form__label">
+    <FieldArray name="subtasks">
+    {({ remove, push }) => (
+      <div className="subtasks">
+        <span className="form__label__title">Subtasks</span>
+        {values.subtasks.length > 0 &&
+          values.subtasks.map((subtask, index) => (
+            <div className="subtask" key={index}>
+              <label className="form__label">
+                <Field
+                  className="form__input"
+                  name={`subtasks.${index}.title`}
+                  type="text"
+                />
+                <ErrorMessage name={`subtasks.${index}.title`}>
+                  {(msg) => <span className="form__error">{msg}</span>}
+                </ErrorMessage>
+              </label>
+              <label className="form__reset subtask__remove">
+                <input type="button" onClick={() => remove(index)} />
+                <Delete />
+              </label>
+            </div>
+          ))}
+        <button
+          type="button"
+          className="subtasks__button"
+          onClick={() =>
+            push({
+              title: "",
+              id: getId(),
+            })
+          }
+        >
+          Add Subtask
+        </button>
+      </div>
+    )}
+  </FieldArray>
+    <label className="form__label form__select">
       <span className="form__label__title">Status</span>
-      <Field className="form__input" name="status" component="select">
-        <option defaultValue='todo' value="todo">Todo</option>
+      <Field
+        className="form__input form__input_select"
+        name="status"
+        component="select"
+      >
+        <option defaultValue="todo" value="todo">
+          Todo
+        </option>
         <option value="doing">Doing</option>
         <option value="done">Done</option>
       </Field>
+      <Select className="form__select-appearance" />
     </label>
-    <FormControls submitTitle={formTitle} onRemove={onRemove} />
-  </Form>
+  </FormWrapper>
 );
 
 export default TaskForm;
--- src/components/TaskForm/TaskFormContainer.jsx
+++ src/components/TaskForm/TaskFormContainer.jsx
@@ -1,16 +1,40 @@
 import { Formik } from "formik";
+import * as Yup from "yup";
 import TaskForm from "./TaskForm";
-import getTaskFormProps from "../../helpers/getTaskFormProps";
+import getId from "../../helpers/getId";
 
-const TaskFormContainer = ({ initialValues, onSubmit, onRemove, ...props }) => {
-  return (
-    <Formik
-      {...getTaskFormProps(initialValues)}
-      onSubmit={(values) => onSubmit(values)}
-    >
-      <TaskForm {...props} onRemove={onRemove} />
-    </Formik>
-  );
-};
+const TaskFormContainer = ({ initialValues, onSubmit, onReset, ...props }) => (
+  <Formik
+    initialValues={{
+      title: "",
+      description: "",
+      checkedSubtasks: [],
+      subtasks: [
+        {
+          title: "",
+          id: getId(),
+        },
+      ],
+      status: "todo",
+      ...initialValues,
+    }}
+    validationSchema={Yup.object({
+      title: Yup.string()
+        .max(30, "Title must be 30 characters or less")
+        .required("Title is required"),
+      subtasks: Yup.array().of(
+        Yup.object({
+          title: Yup.string()
+            .max(20, "Subtask title must be 20 characters or less")
+            .required("Subtask title is required"),
+        })
+      ),
+    })}
+    onSubmit={(values) => onSubmit(values)}
+    onReset={() => onReset()}
+  >
+    {({ values }) => <TaskForm {...{ ...props, values }} />}
+  </Formik>
+);
 
 export default TaskFormContainer;
