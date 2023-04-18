d5f976c665386a9115b8c2ca2799f441637b14cc
Nikimad <PybePoiD@gmail.com>
2023-04-18 15:35:16 +0300

    Refactor /src/components/Headerr

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,60 @@
 import "./Header.scss";
-import CurrentBoard from "../CurrentBoard";
-import AddTask from "../AddTask";
 import LogoToggler from "../LogoToggler";
+import Modal from "../Modal";
+import BoardForm from "../BoardForm";
+import TaskForm from "../TaskForm";
 
-const Header = ({ currentBoard, toggleNav }) => (
+const Header = ({
+  toggleNav,
+  activeBoard,
+  onEditBoard,
+  onRemoveBoard,
+  activeTask,
+  createTask,
+  onEditTask,
+  onRemoveTask,
+}) => (
   <div className="header__wrapper">
-    <header className="header" data-empty={!Boolean(currentBoard)}>
+    <header className={`header${!Boolean(activeBoard) ? " header_empty" : ""}`}>
       <LogoToggler onClick={toggleNav} />
-      {Boolean(currentBoard) ? (
-        <CurrentBoard toggleNav={toggleNav} board={currentBoard} />
-      ) : null}
-      <AddTask />
+      <div className="header__active">
+        <Modal>
+          <h2 className="header__active__title">
+            {Boolean(activeTask) ? activeTask?.title : activeBoard?.title}
+          </h2>
+          {Boolean(activeTask) ? (
+            <TaskForm
+              initialValues={activeTask}
+              isEdit={true}
+              onSubmit={onEditTask}
+              onReset={onRemoveTask}
+              formTitle="Edit task"
+              submitTitle="Edit"
+            />
+          ) : (
+            <BoardForm
+              initialValues={activeBoard}
+              isEdit={true}
+              onSubmit={onEditBoard}
+              onReset={onRemoveBoard}
+              formTitle="Edit board"
+              submitTitle="Edit"
+            />
+          )}
+        </Modal>
+      </div>
+      {Boolean(activeTask) ? null : (
+        <Modal>
+          <button className="header__button">
+            +<span className="header__button__text"> Add New Task</span>
+          </button>
+          <TaskForm
+            formTitle="Add new task"
+            submitTitle="Add task"
+            onSubmit={createTask}
+          />
+        </Modal>
+      )}
     </header>
   </div>
 );
--- src/components/Header/HeaderContainer.jsx
+++ src/components/Header/HeaderContainer.jsx
@@ -1,14 +1,40 @@
-import Header from "./Header";
 import { useSelector } from "react-redux";
-import { selectCurrentBoard } from "../../models/boards/boardsSelectors";
+import useAction from "../../hooks/useAction";
+import Header from "./Header";
+import { selectActiveBoard } from "../../models/boards/boardsSelectors";
+import { selectActiveTask } from "../../models/tasks/tasksSelectors";
+import { editBoard, removeBoard } from "../../models/boards/boardsSlice";
+import { addTask, editTask, removeTask } from "../../models/tasks/tasksSlice";
 
 const HeaderContainer = (props) => {
-  const currentBoard = useSelector(selectCurrentBoard);
+  const activeBoard = useSelector(selectActiveBoard);
+  const activeTask = useSelector(selectActiveTask);
+
+  const dispatchEditBoard = useAction(editBoard);
+  const dispatchRemoveBoard = useAction(removeBoard);
+  const dispatchAddTask = useAction(addTask);
+  const dispatchEditTask = useAction(editTask);
+  const dispatchRemoveTask = useAction(removeTask);
+
+  const onEditBoard = (values) => dispatchEditBoard(values);
+  const onRemoveBoard = () => dispatchRemoveBoard(activeBoard.id);
+  const createTask = (values) =>
+    dispatchAddTask({ boardId: activeBoard.id, ...values });
+  const onEditTask = (values) => dispatchEditTask(values);
+  const onRemoveTask = () => dispatchRemoveTask(activeTask.id);
 
   return (
     <Header
-      { ...props }
-      currentBoard={currentBoard}
+      {...{
+        ...props,
+        activeBoard,
+        onEditBoard,
+        onRemoveBoard,
+        activeTask,
+        createTask,
+        onEditTask,
+        onRemoveTask,
+      }}
     />
   );
 };
