6f4209e60710f190a8fdcd1c7e50dd079059d74f
Nikimad <PybePoiD@gmail.com>
2023-04-18 15:35:59 +0300

    Refactor /src/components/Task

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,43 +1,18 @@
-import Modal from "../Modal";
 import "./Task.scss";
-import { ReactComponent as Edit } from "../../assets/svg/edit.svg";
-import { ReactComponent as Delete } from "../../assets/svg/delete.svg";
-import TaskForm from "../TaskForm";
+import TaskInteractionForm from "../TaskInteractionForm";
 
-const Task = ({
-  taskValues,
-  showModal,
-  modalProps,
-  isEdit,
-  onEdit,
-  onSubmit,
-  onRemove,
-}) => (
-  <li className="task task__preview" onDoubleClick={showModal}>
-    <h3 className="task__title">{taskValues.title}</h3>
-    <Modal {...modalProps}>
-      {isEdit ? (
-        <TaskForm
-          formTitle="Edit Task"
-          initialValues={taskValues}
-          onSubmit={onSubmit}
-          onRemove={onRemove}
-        />
-      ) : (
-        <div className="task task__review">
-          <h3 className="task__title">{taskValues.title}</h3>
-          <div className="task__description">
-            <p><span className="task__description__heading">Status:</span> <span className="task__description__status">{taskValues.status}</span></p>
-            { taskValues.description.length > 0 ? <p className="task__description__main"><span className="task__description__heading">Description</span> {taskValues.description}</p> : null }
-          </div>
-          <div className="task__controls">
-            <Edit onClick={onEdit} />
-            <Delete onClick={onRemove} />
-          </div>
-        </div>
-      )}
-    </Modal>
-  </li>
+const Task = ({ task, onBack }) => (
+  <div className="task">
+    {task.description.length > 0 ? (
+      <p className="task__description">{task.description}</p>
+    ) : null}
+    <TaskInteractionForm {...task} />
+    <div className="task__controls">
+      <button className="task__button" onClick={onBack}>
+        Back
+      </button>
+    </div>
+  </div>
 );
 
 export default Task;
--- src/components/Task/TaskContainer.jsx
+++ src/components/Task/TaskContainer.jsx
@@ -1,45 +1,12 @@
-import useModal from "../../hooks/useModal";
 import Task from "./Task";
-import { useState } from "react";
-import useStateDepAction from "../../hooks/useStateDepAction";
-import { updateTask, removeTask } from "../../models/tasks/tasksSlice";
 import useAction from "../../hooks/useAction";
+import { setActiveTaskId } from "../../models/tasks/tasksSlice";
 
-const TaskContainer = ({ task }) => {
-  const { id, ...taskValues } = task;
-  const { showModal, resetModal, ...modalProps } = useModal();
-  const [isEdit, setIsEdit] = useState(false);
-  const handleEditClick = () => setIsEdit(true);
+const TaskContainer = ({task}) => {
+  const dispatchSetActiveTask = useAction(setActiveTaskId);
+  const onBack = () => dispatchSetActiveTask(null);
 
-  const dispatchStateDepUpdateTask = useStateDepAction(updateTask);
-  const dispatchRemoveTask = useAction(removeTask(id));
-
-  const resetTask = () => {
-    resetModal();
-    setIsEdit(false);
-  };
-
-  const handleUpdateBoard = (values) => {
-    dispatchStateDepUpdateTask({ id, changes: values });
-    setIsEdit(false);
-  };
-
-  const handleRemoveTask = () => {
-    setTimeout(dispatchRemoveTask, 100);
-    resetModal();
-  };
-
-  return (
-    <Task
-      taskValues={taskValues}
-      isEdit={isEdit}
-      showModal={showModal}
-      modalProps={{ ...modalProps, resetModal: resetTask }}
-      onEdit={handleEditClick}
-      onSubmit={handleUpdateBoard}
-      onRemove={handleRemoveTask}
-    />
-  );
+  return <Task { ...{task, onBack}} />;
 };
 
 export default TaskContainer;
