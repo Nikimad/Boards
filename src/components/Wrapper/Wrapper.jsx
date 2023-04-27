ae061fd82299f0b265a467db768cebf7dbee9235
Nikimad <PybePoiD@gmail.com>
2023-04-27 15:32:55 +0300

    Change /src/components/Wrapper: scss -> module.scss

--- src/components/Wrapper/Wrapper.jsx
+++ src/components/Wrapper/Wrapper.jsx
@@ -1,10 +1,13 @@
-import "./Wrapper.scss";
 import PropTypes from "prop-types";
+import s from "./Wrapper.module.scss";
 
-const Wrapper = ({ children }) => <div className="wrapper">{children}</div>;
+const Wrapper = ({ children }) => <div className={s.wrapper}>{children}</div>;
 
 Wrapper.propTypes = {
-  children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.element), PropTypes.element]),
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.element),
+    PropTypes.element,
+  ]),
 };
 
 export default Wrapper;
