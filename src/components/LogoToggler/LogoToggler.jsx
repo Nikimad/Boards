aa2231a502ceb8652903fac53a9086e32336a842
Nikimad <PybePoiD@gmail.com>
2023-05-04 15:40:33 +0300

    Change /src/components/LogoToggler

--- src/components/LogoToggler/LogoToggler.jsx
+++ src/components/LogoToggler/LogoToggler.jsx
@@ -1,9 +1,9 @@
-import "./LogoToggler.scss";
-import { ReactComponent as Logo } from "../../assets/svg/logo.svg";
 import PropTypes from "prop-types";
+import { ReactComponent as Logo } from "../../assets/svg/logo.svg";
+import s from "./LogoToggler.module.scss";
 
 const LogoToggler = ({ onClick, isDisabled }) => (
-  <button className="logo-toggler" onClick={onClick} disabled={isDisabled}>
+  <button className={s.toggler} onClick={onClick} disabled={isDisabled}>
     <Logo />
   </button>
 );
