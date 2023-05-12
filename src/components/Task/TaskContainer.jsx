c74c96abb31c38ebe8e0312665a287c3e2a45b25
Nikimad <PybePoiD@gmail.com>
2023-05-12 13:47:28 +0300

    Change /src/components/Task

--- src/components/Task/TaskContainer.jsx
+++ src/components/Task/TaskContainer.jsx
@@ -2,7 +2,7 @@ import useAction from "../../hooks/useAction";
 import { setActiveTaskId } from "../../models/tasks/tasksSlice";
 import Task from "./Task";
 
-const TaskContainer = ({task}) => {
+const TaskContainer = ({ task }) => {
   const dispatchSetActiveTask = useAction(setActiveTaskId);
   const onBack = () => dispatchSetActiveTask(null);
 
