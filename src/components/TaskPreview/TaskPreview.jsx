156df5a804d3720d2a7ef0e402432647abad3400
Nikimad <PybePoiD@gmail.com>
2023-05-12 13:48:10 +0300

    Change /src/components/TaskPreview

--- src/components/TaskPreview/TaskPreview.jsx
+++ src/components/TaskPreview/TaskPreview.jsx
@@ -4,25 +4,22 @@ import TaskInteractionForm from "../TaskInteractionForm";
 import Modal from "../Modal";
 import s from "./TaskPreview.module.scss";
 
-const TaskPreview = ({
-  task,
-  setActiveTask,
-}) => (
-  <div className={s["task-preview"]}>
-    <button onClick={setActiveTask} className={s["task-preview__content"]}>
-      <h3 className={s["task-preview__content__title"]}>{task.title}</h3>
-      <p className={s["task-preview__content__text"]}>
+const TaskPreview = ({ task, setActiveTask }) => (
+  <div className={s.taskpreview}>
+    <button onClick={setActiveTask} className={s.taskpreview__content}>
+      <h3 className={s.taskpreview__content__title}>{task.title}</h3>
+      <p className={s.taskpreview__content__text}>
         {task.subtasks.length > 0
           ? `${task.checkedSubtasks.length} of ${task.subtasks.length} subtasks`
           : `Status: ${task.status}`}
       </p>
     </button>
     <Modal>
-      <button className={s["task-preview__open-review"]}>
+      <button className={s.taskpreview__openreview}>
         <View />
       </button>
-      <div className={s["task-review"]}>
-        <h2 className={s["task-review__title"]}>{task.title}</h2>
+      <div className={s.taskreview}>
+        <h2 className={s.taskreview__title}>{task.title}</h2>
         <TaskInteractionForm task={task} />
       </div>
     </Modal>
@@ -33,7 +30,9 @@ TaskPreview.propTypes = {
   task: PropTypes.shape({
     id: PropTypes.number,
     status: PropTypes.string,
-    subtasks: PropTypes.arrayOf(PropTypes.shape({ id: PropTypes.number, title: PropTypes.string })),
+    subtasks: PropTypes.arrayOf(
+      PropTypes.shape({ id: PropTypes.number, title: PropTypes.string })
+    ),
     checkedSubtasks: PropTypes.arrayOf(PropTypes.string),
   }),
   setActiveTask: PropTypes.func,
