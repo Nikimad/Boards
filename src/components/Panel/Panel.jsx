4c45276e45d05c3bfcf8f640198e7e0b7ae0c153
Nikimad <PybePoiD@gmail.com>
2023-05-15 14:29:06 +0300

    Refactor /src/components/Panel

--- src/components/Panel/Panel.jsx
+++ src/components/Panel/Panel.jsx
@@ -3,31 +3,90 @@ import cn from "classnames";
 import { ReactComponent as Icon } from "../../assets/svg/doc.svg";
 import TaskPreview from "../TaskPreview";
 import Task from "../Task";
+import {
+  form__label,
+  form__label_noerr,
+  form__input,
+} from "../../styles/form.module.scss";
 import s from "./Panel.module.scss";
 
-const Panel = ({ items, activeTask, activeBoardId }) => (
-  <div className={cn(s.panel, { [s.panel__list]: !Boolean(activeTask) })}>
-    {Boolean(activeTask) ? (
-      <Task task={activeTask} />
-    ) : items.length > 0 ? (
-      items.map((task) => <TaskPreview key={task.id} task={task} />)
+const Panel = ({
+  task,
+  tasks,
+  length,
+  genLength,
+  onChange,
+  query,
+  isBoardChosen,
+  isTaskChosen,
+}) => (
+  <div className={s.panel}>
+    {isTaskChosen ? (
+      <Task task={task} />
     ) : (
-      <div className={s.panel__plug}>
-        <Icon width="3em" />
-        <p>
-          {Boolean(activeBoardId)
-            ? "No tasks on this board yet"
-            : "Choose board"}
-        </p>
-      </div>
+      <>
+        {genLength > 0 ? (
+          <>
+            <form className={s.panel__form}>
+              <label className={cn(form__label, form__label_noerr)}>
+                <input
+                  className={form__input}
+                  value={query}
+                  onChange={onChange}
+                  placeholder="Search"
+                />
+              </label>
+            </form>
+            {length === 0 ? (
+              <div className={s.panel__plug}>
+                <p>No tasks on this board contain: {query}</p>
+              </div>
+            ) : (
+              <div className={s.panel__list}>
+                {tasks.map((task) => (
+                  <TaskPreview key={task.id} task={task} />
+                ))}
+              </div>
+            )}
+          </>
+        ) : (
+          <div className={s.panel__plug}>
+            <Icon width="3em" />
+            <p>
+              {isBoardChosen ? "No tasks on this board yet" : "Choose board"}
+            </p>
+          </div>
+        )}
+      </>
     )}
   </div>
 );
 
 Panel.propTypes = {
-  items: PropTypes.array,
-  activeTask: PropTypes.object,
-  activeBoardId: PropTypes.number,
+  task: PropTypes.shape({
+    id: PropTypes.number,
+    status: PropTypes.string,
+    subtasks: PropTypes.arrayOf(
+      PropTypes.shape({ id: PropTypes.number, title: PropTypes.string })
+    ),
+    checkedSubtasks: PropTypes.arrayOf(PropTypes.string),
+  }),
+  tasks: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+      status: PropTypes.string,
+      subtasks: PropTypes.arrayOf(
+        PropTypes.shape({ id: PropTypes.number, title: PropTypes.string })
+      ),
+      checkedSubtasks: PropTypes.arrayOf(PropTypes.string),
+    })
+  ),
+  length: PropTypes.number,
+  genLength: PropTypes.number,
+  onChange: PropTypes.func,
+  query: PropTypes.string,
+  isBoardChosen: PropTypes.bool,
+  isTaskChosen: PropTypes.bool,
 };
 
 export default Panel;
--- src/components/Panel/PanelContainer.jsx
+++ src/components/Panel/PanelContainer.jsx
@@ -5,17 +5,26 @@ import {
 } from "../../models/tasks/tasksSelectors";
 import { activeBoardIdSelector } from "../../models/boards/boardsSelectors";
 import Panel from "./Panel";
+import { useState } from "react";
 
 const PanelContainer = () => {
-  const items = useSelector(tasksSelector);
-  const activeTask = useSelector(activeTaskSelector);
+  const [query, setQuery] = useState("");
+  const handleChange = (e) => setQuery(e.target.value);
+
   const activeBoardId = useSelector(activeBoardIdSelector);
+  const activeTask = useSelector(activeTaskSelector);
+  const { tasks, length, genLength } = useSelector(tasksSelector(query));
 
   return (
     <Panel
-      items={items}
-      activeTask={activeTask}
-      activeBoardId={activeBoardId}
+      task={activeTask}
+      tasks={tasks}
+      length={length}
+      genLength={genLength}
+      onChange={handleChange}
+      query={query}
+      isBoardChosen={Boolean(activeBoardId)}
+      isTaskChosen={Boolean(activeTask)}
     />
   );
 };
