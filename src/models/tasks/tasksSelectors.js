381fb4e91a9cfd21b3b7563e8d738b1b7c9f6d0c
Nikimad <PybePoiD@gmail.com>
2023-05-15 14:30:52 +0300

    Change /src/models/tasks/*Selectors.js: add tasksSelector

--- src/models/tasks/tasksSelectors.js
+++ src/models/tasks/tasksSelectors.js
@@ -6,18 +6,39 @@ const rootSelector = createSelector(
   (state) => state.tasks
 );
 
-export const tasksSelector = createSelector(
-  rootSelector,
-  activeBoardIdSelector,
-  ({tasks, tasksIds}, boardId) => tasksIds.reduce((acc, id) => {
-    if (tasks[id].boardId === boardId) {
-      acc = [...acc, tasks[id]];
+export const tasksSelector = (query) =>
+  createSelector(
+    rootSelector,
+    activeBoardIdSelector,
+    ({ tasks, tasksIds }, boardId) => {
+      const queryRegEx = new RegExp(query.trim(), "i");
+
+      const actualTasks = tasksIds.reduce((acc, id) => {
+        if (tasks[id].boardId === boardId) {
+          acc.length += 1;
+          const { title, subtasks } = tasks[id];
+          const words = [
+            title,
+            ...subtasks.map((subtask) => subtask.title),
+          ].join(" ");
+
+          acc.tasks = queryRegEx.test(words) ? [...acc.tasks, tasks[id]] : acc.tasks;
+        }
+        return acc;
+      }, {
+        tasks: [],
+        length: 0
+      });
+
+      return {
+        tasks: actualTasks.tasks,
+        length: actualTasks.tasks.length,
+        genLength: actualTasks.length,
+      };
     }
-    return acc;
-  }, [])
-);
+  );
 
 export const activeTaskSelector = createSelector(
   rootSelector,
-  ({tasks, activeTaskId}) => tasks[activeTaskId]
+  ({ tasks, activeTaskId }) => tasks[activeTaskId]
 );
