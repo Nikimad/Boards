a8ad6c4d5f3fafb1233bbbd9434fbbf492f1c6fe
Nikimad <PybePoiD@gmail.com>
2023-06-10 18:36:39 +0300

    Add src/App

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+import { useLocation } from "react-router-dom";
+import HiddableContentContextProvider from "./components/HiddableContentContextProvider";
+import PageRoutes from "./routes/PageRoutes";
+import ModalRoutes from "./routes/ModalRoutes";
 
 const App = () => {
+  const location = useLocation();
+  const previousLocation = location.state?.previousLocation;
 
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
+    <HiddableContentContextProvider>
+      <PageRoutes location={previousLocation || location} />
+      {previousLocation && <ModalRoutes />}
+    </HiddableContentContextProvider>
   );
 };
 
