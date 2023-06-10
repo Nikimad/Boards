1d35dff07e83c73404e1dec3957896ca3163e1e5
Nikimad <PybePoiD@gmail.com>
2023-06-10 18:29:18 +0300

    Refactor /src/components/BoardForm

--- src/components/BoardForm/BoardFormContainer.jsx
+++ src/components/BoardForm/BoardFormContainer.jsx
@@ -1,35 +1,62 @@
-import { Formik } from "formik";
+import { useContext } from "react";
+import HiddableContentContext from "../../context/HiddableContentContext";
+import { useNavigate, useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { boardByIdSelector } from "../../models/boards/boardsSelectors";
+import useAction from "../../hooks/useAction";
+import {
+  addBoard,
+  editBoard,
+  removeBoard,
+} from "../../models/boards/boardsSlice";
 import * as Yup from "yup";
+import { Formik } from "formik";
+import Modal from "../Modal";
 import BoardForm from "./BoardForm";
-import PropTypes from 'prop-types';
 
-const BoardFormContainer = ({ initialValues, onSubmit, onReset, ...props }) => {
-  return (
-    <Formik
-      initialValues={{
-        title: "",
-        ...initialValues,
-      }}
-      validationSchema={Yup.object({
-        title: Yup.string()
-          .min(5, "Title must be 5 characters or more")
-          .max(16, "Title must be 16 characters or less")
-          .required("Title is required"),
-      })}
-      onSubmit={(values) => onSubmit(values)}
-      onReset={() => onReset()}
-    >
-      <BoardForm {...props} />
-    </Formik>
+const BoardFormContainer = () => {
+  const { toggleIsHidden } = useContext(HiddableContentContext);
+
+  const navigate = useNavigate();
+  const { boardId, action } = useParams();
+
+  const board = useSelector(boardByIdSelector(boardId));
+
+  const dispatchSubmitAction = useAction(
+    action === "edit" ? editBoard : addBoard
   );
-};
+  const dispatchRemoveBoard = useAction(removeBoard);
 
-BoardFormContainer.propTypes = {
-  initialValues: PropTypes.shape({
-    title: PropTypes.string
-  }),
-  onSubmit: PropTypes.func,
-  onReset: PropTypes.func,
+  const onSubmit = (values) => dispatchSubmitAction(values);
+  const onReset = () => {
+    navigate("/");
+    toggleIsHidden();
+    dispatchRemoveBoard(board);
+  };
+
+  return (
+    <Modal>
+      <Formik
+        initialValues={
+          board ?? {
+            id: +boardId,
+            title: "",
+            tasksIds: [],
+          }
+        }
+        validationSchema={Yup.object({
+          title: Yup.string()
+            .min(5, "Title must be 5 characters or more")
+            .max(16, "Title must be 16 characters or less")
+            .required("Title is required"),
+        })}
+        onSubmit={onSubmit}
+        onReset={onReset}
+      >
+        <BoardForm isEdit={action === "edit"} />
+      </Formik>
+    </Modal>
+  );
 };
 
 export default BoardFormContainer;
