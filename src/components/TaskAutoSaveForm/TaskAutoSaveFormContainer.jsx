4292202640ebdeb16f515efc8c4cd80231dfb214
Nikimad <PybePoiD@gmail.com>
2023-06-10 18:30:06 +0300

    Refactor /src/components/TaskAutoSaveForm

--- src/components/TaskAutoSaveForm/TaskAutoSaveFormContainer.jsx
+++ src/components/TaskAutoSaveForm/TaskAutoSaveFormContainer.jsx
@@ -1,12 +1,24 @@
-import { useEffect } from "react";
-import PropTypes from "prop-types";
+import { Fragment, useEffect } from "react";
 import useAction from "../../hooks/useAction";
 import { editTask } from "../../models/tasks/tasksSlice";
 import { Formik } from "formik";
 import TaskAutoSaveForm from "./TaskAutoSaveForm";
+import { useLocation, useParams } from "react-router-dom";
+import { taskByIdSelector } from "../../models/tasks/tasksSelectors";
+import { useSelector } from "react-redux";
+import Modal from "../Modal";
 
-const TaskAutoSaveFormContainer = ({ task }) => {
+const TaskAutoSaveFormContainer = () => {
+  const { pathname } = useLocation();
+  const { taskId } = useParams();
+
+  const isTaskPage = !pathname.endsWith("review");
+
+  const task = useSelector(taskByIdSelector(taskId));
+
   const dispatchEditTask = useAction(editTask);
+
+  const Wrapper = isTaskPage ? Fragment : Modal;
 
   return (
     <Formik
@@ -23,21 +35,16 @@ const TaskAutoSaveFormContainer = ({ task }) => {
           submitForm();
         }, [values, submitForm]);
 
-        return <TaskAutoSaveForm subtasks={task.subtasks} />;
+        return (
+          <Wrapper>
+            <TaskAutoSaveForm subtasks={task.subtasks}>
+              {!isTaskPage ? <h2>{task.title}</h2> : null}
+            </TaskAutoSaveForm>
+          </Wrapper>
+        );
       }}
     </Formik>
   );
-};
-
-TaskAutoSaveFormContainer.propTypes = {
-  task: PropTypes.shape({
-    id: PropTypes.number,
-    status: PropTypes.string,
-    subtasks: PropTypes.arrayOf(
-      PropTypes.shape({ id: PropTypes.number, title: PropTypes.string })
-    ),
-    checkedSubtasks: PropTypes.arrayOf(PropTypes.string),
-  }),
 };
 
 export default TaskAutoSaveFormContainer;
