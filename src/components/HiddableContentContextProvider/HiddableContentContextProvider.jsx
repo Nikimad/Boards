a24e4e55b26d965d657fca88b61781200c86ef2a
Nikimad <PybePoiD@gmail.com>
2023-06-11 17:34:55 +0300

    Change /src/components/HiddableContentContextProvider

--- src/components/HiddableContentContextProvider/HiddableContentContextProvider.jsx
+++ src/components/HiddableContentContextProvider/HiddableContentContextProvider.jsx
@@ -2,10 +2,19 @@ import PropTypes from "prop-types";
 import { useState, useCallback, useEffect } from "react";
 import HiddableContentContext from "../../context/HiddableContentContext";
 import debounce from "lodash/debounce";
+import { useLocation } from "react-router-dom";
 
 const HiddableContentContextProvider = ({ children }) => {
+  const location = useLocation();
   const [isHidden, setIsHidden] = useState(false);
   const [isHidable, setIsHidable] = useState(true);
+
+  const handleRootLocation = useCallback(() => {
+    if (isHidden && location.pathname === "/") setIsHidden(false);
+    if (isHidable && !isHidden && location.pathname !== "/") setIsHidden(true);
+  }, [isHidable, location]);
+
+  useEffect(() => handleRootLocation(), [handleRootLocation]);
 
   const handleWindowResize = useCallback(() => {
     if (!isHidable && window.innerWidth <= 800) {
