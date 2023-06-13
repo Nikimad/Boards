401e4e740e0d6431e6363fbfc9178034eb62765f
Nikimad <PybePoiD@gmail.com>
2023-06-13 13:24:10 +0300

    Change /src/components/Task

--- src/components/Task/TaskContainer.jsx
+++ src/components/Task/TaskContainer.jsx
@@ -1,16 +1,20 @@
-import { useNavigate, useParams } from "react-router-dom";
+import { Navigate, useNavigate, useParams } from "react-router-dom";
 import Task from "./Task";
 import { useSelector } from "react-redux";
 import { taskByIdSelector } from "../../models/tasks/tasksSelectors";
 
 const TaskContainer = () => {
-  const { taskId } = useParams();
+  const { boardId, taskId } = useParams();
   const task = useSelector(taskByIdSelector(taskId));
-  
+
   const navigate = useNavigate();
   const onBack = () => navigate(-1);
 
-  return <Task task={task} onBack={onBack} />;
+  return Boolean(task) ? (
+    <Task task={task} onBack={onBack} />
+  ) : (
+    <Navigate to={`/board/${boardId}`} />
+  );
 };
 
 export default TaskContainer;
