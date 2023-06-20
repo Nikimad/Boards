17f6dd59e83a08ef1f4b947922ea84de46e02963
Nikimad <PybePoiD@gmail.com>
2023-06-20 16:21:39 +0300

    Change /src/models/board/boardsSelectors

--- src/models/boards/boardsSelectors.js
+++ src/models/boards/boardsSelectors.js
@@ -5,10 +5,25 @@ const rootSelector = createSelector(
   (state) => state.boards
 );
 
-export const allBoardsSelector = createSelector(
-  rootSelector,
-  ({ boards, boardsIds }) => boardsIds.map((id) => boards[id])
-);
+export const boardsSelector = (filter) =>
+  createSelector(rootSelector, ({ boards, boardsIds }) => {
+    const fitredBoards = boardsIds.reduce((filtredBoards, id) => {
+      const queryRegExp = new RegExp(filter, "i");
+
+      filtredBoards = queryRegExp.test(boards[id].title)
+        ? [...filtredBoards, boards[id]]
+        : filtredBoards;
+      
+      return filtredBoards;
+    }, []);
+
+    return {
+      boards: fitredBoards,
+      length: boardsIds.length,
+      filtredLength: fitredBoards.length,
+    };
+  }
+  );
 
 export const boardByIdSelector = (id) =>
   createSelector(rootSelector, ({ boards }) => boards[id]);
