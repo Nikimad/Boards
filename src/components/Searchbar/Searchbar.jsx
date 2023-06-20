e224ae5b705bb8aae3c6395962188f85dcd2dd2c
Nikimad <PybePoiD@gmail.com>
2023-06-20 16:21:04 +0300

    Change /src/components/Searchbar

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,14 +1,15 @@
 import PropTypes from "prop-types";
+import cn from "classnames";
 import sf from "../../styles/form.module.scss";
 import s from "./Searchbar.module.scss";
 
-const Searchbar = ({ value, onChange }) => (
+const Searchbar = ({ value, onChange, placeholder, className }) => (
   <form className={s.searchbar}>
-    <label className={sf.form__label}>
+    <label className={cn(sf.form__label, className)}>
       <input
         className={sf.form__input}
         type="text"
-        placeholder="Search"
+        placeholder={placeholder}
         value={value}
         onChange={onChange}
       />
--- src/components/Searchbar/SearchbarContainer.jsx
+++ src/components/Searchbar/SearchbarContainer.jsx
@@ -1,12 +1,18 @@
 import { useSearchParams } from "react-router-dom";
 import Searchbar from "./Searchbar";
 
-const SearchbarContainer = () => {
-  const [searchParams, setSearchParams] = useSearchParams({ search: "" });
-  const handleChange = ({ target }) => setSearchParams({ search: target.value});
+const SearchbarContainer = ({ param, placeholder, className }) => {
+  const [searchParams, setSearchParams] = useSearchParams({ [param]: "" });
+  const handleChange = ({ target }) =>
+    setSearchParams({ [param]: target.value });
 
   return (
-    <Searchbar value={searchParams.get("search") ?? ""} onChange={handleChange} />
+    <Searchbar
+      value={searchParams.get(param) ?? ""}
+      onChange={handleChange}
+      placeholder={placeholder}
+      className={className}
+    />
   );
 };
 
