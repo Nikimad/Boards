6ca2b8a3c4d3a0b6f96c0dbf3038d8a4fe644572
Nikimad <PybePoiD@gmail.com>
2023-06-20 15:09:49 +0300

    Change /src/components/TaskForm

--- src/components/TaskForm/TaskFormContainer.jsx
+++ src/components/TaskForm/TaskFormContainer.jsx
@@ -1,64 +1,80 @@
 import { useNavigate, useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { taskByIdSelector } from "../../models/tasks/tasksSelectors";
+import useModal from "../../hooks/useModal";
 import useAction from "../../hooks/useAction";
 import { addTask, editTask, removeTask } from "../../models/tasks/tasksSlice";
 import * as Yup from "yup";
 import { Formik } from "formik";
 import Modal from "../Modal";
 import TaskForm from "./TaskForm";
+import TaskAutoSaveForm from "../TaskAutoSaveForm";
 
 const TaskFormContainer = () => {
+  const modalProps = useModal();
   const navigate = useNavigate();
   const { boardId, taskId, action } = useParams();
 
   const task = useSelector(taskByIdSelector(taskId));
 
   const dispatchSubmitAction = useAction(
-    action === "edit" ? editTask : addTask
+    action === "create" ? addTask : editTask
   );
   const dispatchRemoveTask = useAction(removeTask);
 
-  const onSubmit = (values) => dispatchSubmitAction(values);
+  const onSubmit = (values) => {
+    dispatchSubmitAction(values);
+    if (action !== "review") modalProps.resetModal();
+  };
+
   const onReset = () => {
     dispatchRemoveTask(task);
     navigate(`/board/${boardId}`);
+    modalProps.resetModal();
   };
 
   return (
-    <Modal>
-      <Formik
-        initialValues={
-          task ?? {
-            id: +taskId,
-            boardId: +boardId,
-            title: "",
-            description: "",
-            checkedSubtasks: [],
-            subtasks: [],
-            status: "todo",
-          }
+    <Formik
+      initialValues={
+        task ?? {
+          id: +taskId,
+          boardId: +boardId,
+          title: "",
+          description: "",
+          checkedSubtasks: [],
+          subtasks: [],
+          status: "todo",
         }
-        validationSchema={Yup.object({
-          title: Yup.string()
-            .max(16, "Title must be 16 characters or less")
-            .required("Title is required"),
-          subtasks: Yup.array().of(
-            Yup.object({
-              title: Yup.string()
-                .max(16, "Subtask title must be 16 characters or less")
-                .required("Subtask title is required"),
-            })
-          ),
-        })}
-        onSubmit={onSubmit}
-        onReset={onReset}
-      >
-        {({ values }) => (
-          <TaskForm values={values} isEdit={action === "edit"} />
-        )}
-      </Formik>
-    </Modal>
+      }
+      validationSchema={Yup.object({
+        title: Yup.string()
+          .max(16, "Title must be 16 characters or less")
+          .required("Title is required"),
+        subtasks: Yup.array().of(
+          Yup.object({
+            title: Yup.string()
+              .max(16, "Subtask title must be 16 characters or less")
+              .required("Subtask title is required"),
+          })
+        ),
+      })}
+      onSubmit={onSubmit}
+      onReset={onReset}
+    >
+      {({ values, submitForm }) =>
+        action ? (
+          <Modal modalProps={modalProps}>
+            {action === "review" ? (
+              <TaskAutoSaveForm values={values} submitForm={submitForm} />
+            ) : (
+              <TaskForm values={values} isEdit={action === "edit"} />
+            )}
+          </Modal>
+        ) : (
+          <TaskAutoSaveForm values={values} submitForm={submitForm} />
+        )
+      }
+    </Formik>
   );
 };
 
