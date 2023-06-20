bc06322292dc5a7c75165a8c78c4f86640b42434
Nikimad <PybePoiD@gmail.com>
2023-06-20 15:11:10 +0300

    Change /src/routes/ModalRoutes

--- src/routes/ModalRoutes.jsx
+++ src/routes/ModalRoutes.jsx
@@ -1,17 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import BoardForm from "../components/BoardForm";
 import TaskForm from "../components/TaskForm";
-import TaskAutoSaveForm from "../components/TaskAutoSaveForm";
 
 const ModalRoutes = () => (
   <Routes>
-      <Route path="board/:boardId" >
-        <Route path=":action" element={<BoardForm />} />
-        <Route path="task/:taskId">
-          <Route path="review" element={<TaskAutoSaveForm />} />
-          <Route path=":action" element={<TaskForm />} />
-        </Route>
-      </Route>
+    <Route path="board/:boardId">
+      <Route path=":action" element={<BoardForm />} />
+      <Route path="task/:taskId:action" element={<TaskForm />} />
+    </Route>
   </Routes>
 );
 
