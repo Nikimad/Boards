41823e03487b9d6066e5e9214d180b5ddf436b94
Nikimad <PybePoiD@gmail.com>
2023-06-21 11:49:41 +0300

    Change /vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "",
+        target: "https://boards-db.vercel.app",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
