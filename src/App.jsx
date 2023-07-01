67554a24de8c02b542df9b14aaf6bc5f6c105178
Nikimad <PybePoiD@gmail.com>
2023-07-01 20:20:34 +0300

    Change /src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
+import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { fetchBoards } from "./models/boards/boardsSlice";
+import { fetchTasks } from "./models/tasks/tasksSlice";
 import HiddableContentContextProvider from "./components/HiddableContentContextProvider";
 import PageRoutes from "./routes/PageRoutes";
 import ModalRoutes from "./routes/ModalRoutes";
@@ -6,6 +10,13 @@ import ModalRoutes from "./routes/ModalRoutes";
 const App = () => {
   const location = useLocation();
   const previousLocation = location.state?.previousLocation;
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchBoards());
+    dispatch(fetchTasks());
+  }, [dispatch, fetchBoards, fetchTasks]);
 
   return (
     <HiddableContentContextProvider>
