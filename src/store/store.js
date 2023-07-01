f6d0017007bce8f688a6b0579b056571a5b038e8
Nikimad <PybePoiD@gmail.com>
2023-07-01 20:20:11 +0300

    Change /src/store/*.js

--- src/store/store.js
+++ src/store/store.js
@@ -1,19 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import boards from "../models/boards/boardsSlice";
 import tasks from "../models/tasks/tasksSlice";
-import { loadState, saveState } from "../helpers/localStorageHandlers";
-import throttle from "lodash/throttle";
-
-const state = loadState();
 
 export const store = configureStore({
   reducer: {
     boards,
     tasks,
   },
-  preloadedState: state,
 });
-
-store.subscribe(
-  throttle(() => saveState(store.getState()), 1000, { leading: false })
-);
