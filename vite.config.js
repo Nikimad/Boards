808478a9ec65a033720046d0d2051464d9e329bb
Nikimad <PybePoiD@gmail.com>
2023-07-05 17:40:41 +0300

    Change /vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,5 @@ import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://my-json-server.typicode.com/nikimad/boards-db",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
-  },
   plugins: [svgr(), react()],
 });
