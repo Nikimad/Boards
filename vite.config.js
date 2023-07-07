827132c9d698c237738e7abeb724c827ab69cd56
Nikimad <PybePoiD@gmail.com>
2023-07-07 15:29:46 +0300

    Change /vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,14 @@ import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
   plugins: [svgr(), react()],
 });
