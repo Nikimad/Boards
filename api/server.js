d30b85a50df360a4eae78f9a9657832e11e42d39
Nikimad <PybePoiD@gmail.com>
2023-07-07 14:27:28 +0300

    Change /api/server.js

--- api/server.js
+++ api/server.js
@@ -2,7 +2,14 @@ import jsonServer from "json-server";
 import fs from "fs";
 
 export const server = jsonServer.create();
+
 const db = JSON.parse(fs.readFileSync("db.json"));
 const router = jsonServer.router(db);
+
+server.use(
+  jsonServer.rewriter({
+    "/api/*": "/$1",
+  })
+);
 server.use(router);
 server.listen(3000);
