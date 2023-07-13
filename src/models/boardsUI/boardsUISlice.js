4f519fd8c6e334b59d8526c3f014eae6890b5c16
Nikimad <PybePoiD@gmail.com>
2023-07-13 20:22:31 +0300

    Change /src/models/boardsUI

--- src/models/boardsUI/boardsUISlice.js
+++ src/models/boardsUI/boardsUISlice.js
@@ -1,5 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { fetchBoards } from "../boardsDomain/boardsDomainThunks";
+import { fetchBoards, addBoard } from "../boardsDomain/boardsDomainThunks";
 
 const boardsUISlice = createSlice({
   name: "boardsUI",
@@ -7,9 +7,13 @@ const boardsUISlice = createSlice({
     ids: [],
   },
   extraReducers: (builder) =>
-    builder.addCase(fetchBoards.fulfilled, (state, { payload }) => {
-      state.ids = payload.map(({ id }) => id);
-    })
+    builder
+      .addCase(fetchBoards.fulfilled, (state, { payload }) => {
+        state.ids = payload.map(({ id }) => id);
+      })
+      .addCase(addBoard.fulfilled, (state, { payload }) => {
+        state.ids = [ payload.id, ...state.ids];
+      })
 });
 
 export default boardsUISlice.reducer;
