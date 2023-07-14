ede647e73c71d446b52976e249b1f7419dc0c08c
Nikimad <PybePoiD@gmail.com>
2023-07-14 17:27:32 +0300

    Change /src/models/boardsUI/*Slice

--- src/models/boardsUI/boardsUISlice.js
+++ src/models/boardsUI/boardsUISlice.js
@@ -1,10 +1,18 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { fetchBoards, addBoard } from "../boardsDomain/boardsDomainThunks";
+import isRequested from "../../helpers/isRequsted";
 
 const boardsUISlice = createSlice({
   name: "boardsUI",
   initialState: {
+    searchParams: "",
     ids: [],
+  },
+  reducers: {
+    setSearchParams(state, { payload }) {
+      console.log(payload)
+      state.searchParams = payload;
+    }
   },
   extraReducers: (builder) =>
     builder
@@ -12,8 +20,12 @@ const boardsUISlice = createSlice({
         state.ids = payload.map(({ id }) => id);
       })
       .addCase(addBoard.fulfilled, (state, { payload }) => {
-        state.ids = [ payload.id, ...state.ids];
+        const { board, searchParams } = payload;
+        if (isRequested(board, searchParams)) {
+          state.ids = [payload.board.id, ...state.ids];
+        }
       })
 });
 
+export const { setSearchParams } = boardsUISlice.actions;
 export default boardsUISlice.reducer;
