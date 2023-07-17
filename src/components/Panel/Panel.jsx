ed96559e3659f4ce81b4e60b4f62bc7402d776cc
Nikimad <PybePoiD@gmail.com>
2023-07-17 16:43:51 +0300

    Change /src/components/Panel

--- src/components/Panel/Panel.jsx
+++ src/components/Panel/Panel.jsx
@@ -1,11 +1,7 @@
 import PropTypes from "prop-types";
 import s from "./Panel.module.scss";
 
-const Panel = ({children}) => (
-  <div className={s.panel}>
-    {children}
-  </div>
-);
+const Panel = ({ children }) => <div className={s.panel}>{children}</div>;
 
 Panel.propTypes = {
   children: PropTypes.element,
