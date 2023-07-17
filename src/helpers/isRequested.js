302305ed29ca0ea4f3dc0a22581c0ae22a174c31
Nikimad <PybePoiD@gmail.com>
2023-07-17 16:34:38 +0300

    Change /src/helpers/isRequested.js

--- src/helpers/isRequested.js
+++ src/helpers/isRequested.js
@@ -1,14 +1,14 @@
 const getObjText = (obj) =>
   Object.values(obj).reduce((text, val) => {
-    text += typeof val === "string" ? val.toLowerCase().trim() : getObjText(val)
+    text += typeof val === "string" ? `${val.toLowerCase().trim()} ` : getObjText(val)
     return text;
   }, "");
 
-const isRequsted = (obj, searchParams) => {
+const isRequested = (obj, searchParams) => {
   if (!searchParams) return true;
   const txt = getObjText(obj);
   const regExp = new RegExp(searchParams, "i");
   return regExp.test(txt);
 };
 
-export default isRequsted;
+export default isRequested;
