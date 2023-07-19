4c67c28dde2eee256b79879e5b846971c730d2d9
Nikimad <PybePoiD@gmail.com>
2023-07-19 15:36:35 +0300

    Change /src/hooks/useAction.js

--- src/hooks/useAction.js
+++ src/hooks/useAction.js
@@ -3,7 +3,10 @@ import { useDispatch } from "react-redux";
 
 const useAction = (action) => {
   const dispatch = useDispatch();
-  const dispatchAction = useCallback((state) => dispatch(action(state)), [dispatch, action]);
+  const dispatchAction = useCallback(
+    (state) => dispatch(action(state)),
+    [dispatch, action]
+  );
   return dispatchAction;
 };
 
