809b27779e4c377d24b0aa0cbc7b0d7ad15deeb3
Nikimad <PybePoiD@gmail.com>
2023-07-19 15:27:25 +0300

    Change /src/components/Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,31 +3,51 @@ import { ReactComponent as Logo } from "../../assets/svg/logo.svg";
 import { Link } from "react-router-dom";
 import BoardLink from "../BoardLink";
 import Searchbar from "../Searchbar";
+import Loader from "../Loader";
 import s from "./Navbar.module.scss";
 
-const Navbar = ({ isHidden, boards, query, totalBoards, previousLocation }) => (
+const Navbar = ({
+  isHidden,
+  boards,
+  totalBoards,
+  searchParams,
+  previousLocation,
+  isLoading,
+  isErr,
+}) => (
   <div className={s.navbar} aria-hidden={isHidden}>
     <div className={s.navbar__header}>
       <Logo />
       <h1 className={s.navbar__header__title}>Boards</h1>
-      <p className={s.navbar__header__text}>{`${
-        Boolean(query) ? "Found boards" : "All boards"
-      } (${boards.length})`}</p>
+      <p className={s.navbar__header__text}>
+        {Boolean(searchParams) ? "Found boards" : "All boards"}(
+        {isLoading ? <Loader className={s.navbar__loader} /> : boards.length})
+      </p>
     </div>
-    <Searchbar
-      param="board"
-      placeholder="Search board"
-      className={s.navbar__search}
-      isDisabled={!Boolean(query) && totalBoards === 0}
-    />
-    <nav className={s.navbar__nav}>
-      {boards.map((board) => (
-        <BoardLink key={board.id} board={board} />
-      ))}
-    </nav>
-    <Link to="create/board" state={previousLocation} className={s.navbar__link}>
-      + Create New Board
-    </Link>
+    {isErr ? (
+      <p className={s.navbar__err}>Oops something went wrong</p>
+    ) : (
+      <>
+        <Searchbar
+          param="board"
+          placeholder="Search board"
+          className={s.navbar__search}
+          isDisabled={!Boolean(searchParams) && totalBoards === 0}
+        />
+        <nav className={s.navbar__nav}>
+          {boards.map((board) => (
+            <BoardLink key={board.id} board={board} />
+          ))}
+        </nav>
+        <Link
+          to="create/board"
+          state={previousLocation}
+          className={s.navbar__link}
+        >
+          + Create New Board
+        </Link>
+      </>
+    )}
   </div>
 );
 
@@ -40,7 +60,8 @@ Navbar.propTypes = {
       createdAt: PropTypes.number,
     })
   ),
-  path: PropTypes.string,
+  totalBoards: PropTypes.number,
+  searchParams: PropTypes.oneOfType([PropTypes.shape(null), PropTypes.string]),
   previousLocation: PropTypes.shape({
     previousLocation: PropTypes.shape({
       pathname: PropTypes.string,
@@ -50,6 +71,8 @@ Navbar.propTypes = {
       key: PropTypes.string,
     }),
   }),
+  isLoading: PropTypes.bool,
+  isErr: PropTypes.bool,
 };
 
 export default Navbar;
--- src/components/Navbar/NavbarContainer.jsx
+++ src/components/Navbar/NavbarContainer.jsx
@@ -1,13 +1,12 @@
 import { useEffect } from "react";
 import { useLocation, useSearchParams } from "react-router-dom";
 import { useContext } from "react";
-import HiddableContentContext from "../../context/HiddableContentContext";
 import useAction from "../../hooks/useAction";
 import useDebouncedCallback from "../../hooks/useDebounceCallback";
 import { useSelector } from "react-redux";
-import { boardsDomainSelectors } from "../../models/boardsDomain/boardsDomainSelectors";
-import { boardsUISelectors } from "../../models/boardsUI/boardsUISelectors";
-import { fetchBoards } from "../../models/boardsDomain/boardsDomainThunks";
+import { boardsSelectors } from "../../models/boards/boardsSelectors";
+import { getBoards } from "../../models/boards/boardsThunks";
+import HiddableContentContext from "../../context/HiddableContentContext";
 import Navbar from "./Navbar";
 
 const NavbarContainer = () => {
@@ -16,32 +15,36 @@ const NavbarContainer = () => {
   const location = useLocation();
   const [searchParams] = useSearchParams();
 
-  const query = searchParams.get("board");
+  const boardSearchParams = searchParams.get("board");
 
-  const boardsIds = useSelector(boardsUISelectors.selectAll);
-  const boards = useSelector(boardsDomainSelectors.selectByIds(boardsIds));
-  const totalBoards = useSelector(boardsDomainSelectors.selectTotal);
+  const visibleBoardsIds = useSelector(boardsSelectors.selectVisibleIds);
+  const boards = useSelector(boardsSelectors.selectByIds(visibleBoardsIds));
+  const totalBoards = useSelector(boardsSelectors.selectTotal);
+  const boardsLoadingStatus = useSelector(boardsSelectors.selectLoadingStatus);
+  const boardsError = useSelector(boardsSelectors.selectError);
 
-  const dispatchFetchBoards = useAction(fetchBoards);
-  const debouncedDispatchFetchBoards = useDebouncedCallback(dispatchFetchBoards, 250);
+  const dispatchGetBoards = useAction(getBoards);
+  const debouncedDispatchGetBoards = useDebouncedCallback(
+    dispatchGetBoards,
+    250
+  );
 
   useEffect(() => {
-    debouncedDispatchFetchBoards(query);
+    debouncedDispatchGetBoards(boardSearchParams);
     return () => {
-      debouncedDispatchFetchBoards.cancel();
+      debouncedDispatchGetBoards.cancel();
     };
-  }, [
-    debouncedDispatchFetchBoards,
-    query,
-  ]);
+  }, [debouncedDispatchGetBoards, boardSearchParams]);
 
   return (
     <Navbar
       isHidden={isHidden}
       boards={boards}
-      query={query}
       totalBoards={totalBoards}
+      searchParams={boardSearchParams}
       previousLocation={{ previousLocation: location }}
+      isLoading={boardsLoadingStatus === "loading"}
+      isErr={Boolean(boardsError)}
     />
   );
 };
