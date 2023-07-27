970263e540b054fe1928b9bd6dddb5ddb56f9a44
Nikimad <PybePoiD@gmail.com>
2023-07-27 17:41:02 +0300

    Change /src/components/Plug

--- src/components/Plug/Plug.jsx
+++ src/components/Plug/Plug.jsx
@@ -1,24 +1,29 @@
 import PropTypes from "prop-types";
 import { ReactComponent as Icon } from "../../assets/svg/doc.svg";
-import Loader from "../Loader/Loader";
+import { ReactComponent as Err } from "../../assets/svg/err.svg";
+import Loader from "../Loader";
 import s from "./Plug.module.scss";
 
-const Plug = ({ message, isLoading }) => (
+const Plug = ({ message, isLoading, isError }) => (
   <div className={s.plug}>
-    {isLoading ? (
+    {isError ? 
+    <>
+      <Err />
+      <p>Oops something went wrong</p>
+    </> :
+    isLoading ? (
       <Loader />
     ) : (
-      <>
-        <Icon />
-        <p>{message}</p>
-      </>
+      <Icon />
     )}
+    <p>{message}</p>
   </div>
 );
 
 Plug.propTypes = {
   message: PropTypes.string,
   isLoading: PropTypes.bool,
+  isError: PropTypes.bool,
 };
 
 export default Plug;
