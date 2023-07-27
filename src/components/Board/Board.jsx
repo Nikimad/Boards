af6b3251d9994259bcdcf3beddc2d4a4b25ea90b
Nikimad <PybePoiD@gmail.com>
2023-07-27 17:51:47 +0300

    Change /src/components/Board

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -4,25 +4,21 @@ import Searchbar from "../Searchbar";
 import TaskPreview from "../TaskPreview";
 import s from "./Board.module.scss";
 
-const Board = ({ tasks, searchParams, isLoading, isErr }) =>
-  isErr ? (
-    <Plug message="Oops something went wrong" />
-  ) : !Boolean(searchParams) && tasks.length === 0 ? (
-    <Plug isLoading={isLoading} message="No tasks on this board yet" />
-  ) : (
-    <>
-      <Searchbar param="task" placeholder="Search task" />
-      {tasks.length > 0 ? (
-        <div className={s.board}>
-          {tasks.map((task) => (
-            <TaskPreview key={task.id} task={task} />
-          ))}
-        </div>
-      ) : (
-        <Plug message="Can't find searched tasks" />
-      )}
-    </>
-  );
+const Board = ({ tasks, searchParams }) => (
+  <div className={s.board__wrapper}>
+    <Searchbar param="task" placeholder="Search task" />
+    { tasks.length > 0 ?
+      <div className={s.board}>
+        {tasks.map((task) => (
+          <TaskPreview key={task.id} task={task} />
+        ))}
+      </div> :
+      Boolean(searchParams) ?  
+        (<Plug message="Can't find requested tasks" />) : 
+        (<Plug message="No tasks on this board yet" />)
+    }
+  </div>
+);
 
 Board.propTypes = {
   tasks: PropTypes.arrayOf(
@@ -36,8 +32,6 @@ Board.propTypes = {
     })
   ),
   searchParams: PropTypes.oneOfType([PropTypes.shape(null), PropTypes.string]),
-  isLoading: PropTypes.bool,
-  isErr: PropTypes.bool,
 };
 
 export default Board;
