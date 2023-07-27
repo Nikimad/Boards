96d8bae349c9e15bfee94a57e9098866922d2e1c
Nikimad <PybePoiD@gmail.com>
2023-07-27 17:38:54 +0300

    Change /src/components/Loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,6 +1,6 @@
 import PropTypes from "prop-types";
 import cn from "classnames";
-import { ReactComponent as LoaderIcon } from "../../assets/svg/loading.svg";
+import { ReactComponent as LoaderIcon } from "../../assets/svg/loader.svg";
 import s from "./Loader.module.scss";
 
 const Loader = ({ className }) => <LoaderIcon className={cn(s.loader, className)} />
