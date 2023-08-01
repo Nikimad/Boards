419ee8c8857c48d8bf81a2957a4427e3515d2bf7
Nikimad <PybePoiD@gmail.com>
2023-08-01 16:44:57 +0300

    Change /src/pages/TaskModalPage

--- src/pages/TaskModalPage/EditTaskPage/EditTaskPage.jsx
+++ src/pages/TaskModalPage/EditTaskPage/EditTaskPage.jsx
@@ -1,17 +1,17 @@
 import { useParams } from "react-router-dom";
-import { useDeleteTaskMutation, useGetTaskQuery, usePatchTaskMutation } from "../../../redux/services/tasksApi";
+import { useDeleteTaskMutation, useGetTasksQuery, usePatchTaskMutation } from "../../../redux/services/tasksApi";
 import TaskForm from "../../../components/TaskForm";
-import Plug from "../../../components/Plug/Plug";
 
 const EditTaskPage = () => {
-  const { taskId } = useParams();
-  const { data, isLoading, isError } = useGetTaskQuery(taskId);
+  const { boardId, taskId } = useParams();
+  const { task } = useGetTasksQuery({ boardId }, {
+    selectFromResult: ({ data }) => ({ task: data?.find(({ id }) => id === Number(taskId)) }),
+  });
+
   const [deleteTask] = useDeleteTaskMutation();
   const [patchTask] = usePatchTaskMutation();
 
-  return isError ? <Navigate to="/error" /> :
-    isLoading ? <Plug isLoading={true} /> :
-    <TaskForm onSubmit={patchTask} onDelete={deleteTask} task={data} />
+  return <TaskForm onSubmit={patchTask} onDelete={deleteTask} task={task} />
 };
 
 export default EditTaskPage;
