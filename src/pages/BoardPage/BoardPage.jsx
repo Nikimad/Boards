38f673e386210e06e2476f44818fbe95b8e7872d
Nikimad <PybePoiD@gmail.com>
2023-08-02 12:20:12 +0300

    Change /src/components/BoardPage

--- src/pages/BoardPage/BoardPage.jsx
+++ src/pages/BoardPage/BoardPage.jsx
@@ -1,34 +1,37 @@
 import { useParams, Navigate, useSearchParams } from "react-router-dom";
-import { useGetBoardQuery } from "../../redux/services/boardsApi";
 import { useGetTasksQuery } from "../../redux/services/tasksApi";
-import Header from "../../components/Header";
+import { useGetBoardsQuery } from "../../redux/services/boardsApi";
 import Board from "../../components/Board";
 import Plug from "../../components/Plug/Plug";
+import Header from "../../components/Header";
 
 const BoardPageContainer = () => {
   const { boardId } = useParams();
   const [searchParams] = useSearchParams();
   const taskSearchParams = searchParams.get("task");
-
-  const { 
-    data: board, 
-    isLoading: isLoadingBoard, 
-    isError: isErrorBoard 
-  } = useGetBoardQuery(boardId);
-
-  const { 
-    data: tasks = [],
-    isLoading: isLoadingTasks,
-    isError: isErrorTasks,
+  const {
+    data = [],
+    isLoading,
+    isError,
   } = useGetTasksQuery({ boardId, searchParams: taskSearchParams });
 
-  return (
-    isErrorBoard || isErrorTasks ? (<Navigate to="/error" />) :
-    isLoadingBoard || isLoadingTasks ? <Plug isLoading={true} message={`Fetch ${isLoadingBoard ? "board" : "tasks"}`} /> : 
-      <>
-        <Header board={board} />
-        <Board tasks={tasks} searchParams={taskSearchParams} />
-      </>
+  const { board } = useGetBoardsQuery(undefined, {
+    selectFromResult: ({ data }) => ({
+      board: data?.find(({ id }) => id === Number(boardId)),
+    }),
+  });
+
+  return isError ? (
+    <Navigate to="/error" />
+  ) : isLoading ? (
+    <Plug isLoading={true} message="Fetch tasks" />
+  ) : Boolean(board) ? (
+    <>
+      <Header board={board} />
+      <Board tasks={data} searchParams={taskSearchParams} />
+    </>
+  ) : (
+    <Navigate to="/error" />
   );
 };
 
