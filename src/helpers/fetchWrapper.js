04a83e11422cffb6a7480164dc052e8845248499
Nikimad <PybePoiD@gmail.com>
2023-09-01 14:46:33 +0300

    Add /src/helpers/fetchWrapper.js

---
+++ src/helpers/fetchWrapper.js
@@ -0,0 +1,38 @@
+const handleFetch = async (...args) => {
+  const res = await fetch(...args);
+  if (!res.ok) throw new Error();
+  if (res.ok) return await res.json();
+};
+
+const fetchWrapper = (path) => {
+  const baseUrl = `/api/${path}`;
+  const methods = {
+    post: "POST",
+    patch: "PATCH",
+    delete: "DELETE",
+  };
+  const headers = {
+    "Content-Type": "application/json",
+  };
+  return {
+    get: (url) => handleFetch(`${baseUrl}${Boolean(url) ? `/${url}` : ""}`),
+    post: (body) =>
+      handleFetch(baseUrl, {
+        method: methods.post,
+        headers,
+        body: JSON.stringify(body),
+      }),
+    patch: (url, body) =>
+      handleFetch(`${baseUrl}/${url}`, {
+        method: methods.patch,
+        headers,
+        body: JSON.stringify(body),
+      }),
+    delete: (url) =>
+      handleFetch(`${baseUrl}/${url}`, {
+        method: methods.delete,
+      }),
+  };
+};
+
+export default fetchWrapper;
