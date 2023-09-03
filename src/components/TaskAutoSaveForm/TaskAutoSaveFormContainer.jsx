6f9ae5e560c27388a282853c0780edc2d65be88f
Nikimad <PybePoiD@gmail.com>
2023-09-03 13:43:59 +0300

    Change /src/components/TaskAutoSaveForm

--- src/components/TaskAutoSaveForm/TaskAutoSaveFormContainer.jsx
+++ src/components/TaskAutoSaveForm/TaskAutoSaveFormContainer.jsx
@@ -3,12 +3,12 @@ import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import TaskAutoSaveForm from "./TaskAutoSaveForm";
 
-const TaskAutoSaveFormContainer = ({ values, submitForm }) => {
+const TaskAutoSaveFormContainer = ({ initialValues, values, submitForm }) => {
   const { action } = useParams();
 
   useEffect(() => {
-    submitForm();
-  }, [values, submitForm]);
+    if (initialValues !== values) submitForm();
+  }, [initialValues, values, submitForm]);
 
   return (
     <TaskAutoSaveForm subtasks={values.subtasks}>
@@ -18,6 +18,17 @@ const TaskAutoSaveFormContainer = ({ values, submitForm }) => {
 };
 
 TaskAutoSaveFormContainer.propTypes = {
+  initialValues: PropTypes.shape({
+    title: PropTypes.string,
+    id: PropTypes.number,
+    boardId: PropTypes.number,
+    subtasks: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.number,
+        title: PropTypes.string,
+      })
+    ),
+  }),
   values: PropTypes.shape({
     title: PropTypes.string,
     id: PropTypes.number,
