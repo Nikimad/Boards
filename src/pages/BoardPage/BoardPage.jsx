86fe8d61dea75529be174edadd6c471fec4472d7
Nikimad <PybePoiD@gmail.com>
2023-09-03 13:41:16 +0300

    Change /src/pages/BoardPage

--- src/pages/BoardPage/BoardPage.jsx
+++ src/pages/BoardPage/BoardPage.jsx
@@ -1,40 +1,47 @@
-import { useParams, Navigate, useSearchParams } from "react-router-dom";
-import { useGetTasksQuery } from "../../redux/services/tasksApi";
-import { useGetBoardQuery } from "../../redux/services/boardsApi";
+import { useEffect } from "react";
+import { useNavigate, useParams, useSearchParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import useAction from "../../hooks/useAction";
+import {
+  boardsSelectors,
+  fetchBoard,
+} from "../../redux/slices/boards/boardsSlice";
+import {
+  fetchTasks,
+  tasksSelectors,
+} from "../../redux/slices/tasks/tasksSlice";
 import Board from "../../components/Board";
-import Plug from "../../components/Plug/Plug";
-import Header from "../../components/Header";
 
-const BoardPageContainer = () => {
+const BoardPage = () => {
   const { boardId } = useParams();
+  const navigate = useNavigate();
   const [searchParams] = useSearchParams();
+
   const taskSearchParams = searchParams.get("task");
 
-  const {
-    data = [],
-    isLoading: isTasksLoading,
-    isError: isTasksError,
-  } = useGetTasksQuery({ boardId, searchParams: taskSearchParams });
+  const getBoard = useAction(fetchBoard);
+  const getTasks = useAction(fetchTasks);
 
-  const {
-    data: board,
-    isLoading: isBoardLoading,
-    isError: isBoardError,
-  } = useGetBoardQuery(boardId);
-
-  return isTasksError || isBoardError ? (
-    <Navigate to="/error" />
-  ) : isTasksLoading || isBoardLoading ? (
-    <Plug
-      isLoading={true}
-      message={`Fetch ${Boolean(isTasksLoading) ? "tasks" : "board"}`}
-    />
-  ) : (
-    <>
-      <Header board={board} />
-      <Board tasks={data} searchParams={taskSearchParams} />
-    </>
+  const board = useSelector((state) =>
+    boardsSelectors.selectById(state, boardId)
   );
+
+  const isBoardsError = useSelector(boardsSelectors.selectIsError);
+  const isTasksError = useSelector(tasksSelectors.selectIsError);
+
+  useEffect(() => {
+    if (!Boolean(board)) getBoard(boardId);
+  }, [board, getBoard, boardId]);
+
+  useEffect(() => {
+    getTasks({ boardId, searchParams: taskSearchParams });
+  }, [boardId, getTasks, taskSearchParams]);
+
+  useEffect(() => {
+    if (isBoardsError || isTasksError) navigate("/error");
+  }, [isBoardsError, isTasksError, navigate]);
+
+  return <Board />;
 };
 
-export default BoardPageContainer;
+export default BoardPage;
