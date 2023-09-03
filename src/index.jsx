1bd0961fea994d908b5725919154d813d4363ea5
Nikimad <PybePoiD@gmail.com>
2023-09-03 13:38:02 +0300

    Change /src/index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -7,7 +7,7 @@ import App from "./App";
 import "./styles/global.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+ <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
         <App />
