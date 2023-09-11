85ba5fb64b465d18167ef5a5874a87333158c902
Nikimad <PybePoiD@gmail.com>
2023-09-11 11:44:51 +0300

    Remove /src/hooks/useDebounceCallback.js

--- src/hooks/useDebounceCallback.js
+++
@@ -1,7 +0,0 @@
-import { useCallback } from "react";
-import debounce from "lodash/debounce";
-
-const useDebouncedCallback = (calle, ms, options) =>
-  useCallback(debounce(calle, ms, options), [debounce]);
-
-export default useDebouncedCallback;
