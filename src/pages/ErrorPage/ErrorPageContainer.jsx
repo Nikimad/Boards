9de795497e13acc004ed595768f060c87725030b
Nikimad <PybePoiD@gmail.com>
2023-09-13 15:01:42 +0300

    Change /src/pages/ErrorPage

--- src/pages/ErrorPage/ErrorPageContainer.jsx
+++ src/pages/ErrorPage/ErrorPageContainer.jsx
@@ -2,15 +2,15 @@ import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import useAction from "../../hooks/useAction";
-import { resetIsError as resetIsErrorBoardsAction, boardsSelectors } from "../../redux/slices/boards/boardsSlice";
-import { resetIsError as resetIsErrorTasksAction, tasksSelectors } from "../../redux/slices/tasks/tasksSlice";
+import { boardsActions, boardsSelectors } from "../../redux/slices/boards/boardsSlice";
+import { tasksActions, tasksSelectors } from "../../redux/slices/tasks/tasksSlice";
 import ErrorPage from "./ErrorPage";
 
 const ErrorPageContainer = () => {
   const navigate = useNavigate();
 
-  const resetBoardsError = useAction(resetIsErrorBoardsAction);
-  const resetTasksError = useAction(resetIsErrorTasksAction);
+  const resetBoardsError = useAction(boardsActions.resetIsError);
+  const resetTasksError = useAction(tasksActions.resetIsError);
   
   const boardsError = useSelector(boardsSelectors.selectError);
   const tasksError = useSelector(tasksSelectors.selectError);
