c737ae54f2b4916b5e0d5a7fa71985779daa268a
Nikimad <PybePoiD@gmail.com>
2023-09-13 15:34:37 +0300

    Change /src/redux/slices/tasks/*Slice.js

--- src/redux/slices/tasks/tasksSlice.js
+++ src/redux/slices/tasks/tasksSlice.js
@@ -19,14 +19,36 @@ const tasksSlice = createSlice({
     error: null,
   }),
   reducers: {
-    fetchTasks(state) {
+    getTasks(state) {
       state.isLoading = true;
     },
-    fetchTasksSuccess(state, { payload }) {
+    getTasksSuccess(state, { payload }) {
       adapter.addMany(state, payload);
       const visibleIds = payload.map(({ id }) => id);
       state.visibleIds = visibleIds.sort((a, b) => b - a);
       state.isLoading = false;
+    },
+    getTask(state) {
+      return state;
+    },
+    getTaskSuccess: adapter.addOne,
+    createTask(state) {
+      return state;
+    },
+    editTask(state, { payload }) {
+      payload.values.checkedSubtasks = payload.values.checkedSubtasks.filter(
+        (id) =>
+          payload.values.subtasks.some((subtask) => String(subtask.id) === id)
+      );
+      return state;
+    },
+    editTaskSuccess: adapter.updateOne,
+    deleteTask(state) {
+      return state;
+    },
+    deleteTaskSuccess(state, { payload }) {
+      adapter.removeOne(state, payload);
+      state.visibleIds = state.visibleIds.filter((id) => id !== payload);
     },
     setIsError(state, { payload }) {
       state.error = payload;
@@ -36,29 +58,10 @@ const tasksSlice = createSlice({
       state.error = null;
       state.isError = false;
     },
-    addTask: adapter.addOne,
-    updateTask: adapter.updateOne,
-    removeTask(state, { payload }) {
-      adapter.removeOne(state, payload);
-      state.visibleIds = state.visibleIds.filter((id) => id !== payload);
-    },
   },
 });
 
-export const fetchTask = createAction(`${tasksSlice.name}/fetchTask`);
-export const postTask = createAction(`${tasksSlice.name}/postTask`);
-export const patchTask = createAction(`${tasksSlice.name}/patchTask`);
-export const deleteTask = createAction(`${tasksSlice.name}/deleteTask`);
-
-export const {
-  fetchTasks,
-  fetchTasksSuccess,
-  addTask,
-  updateTask,
-  removeTask,
-  setIsError,
-  resetIsError,
-} = tasksSlice.actions;
+export const tasksActions = tasksSlice.actions;
 
 export const tasksSelectors = getSelectors(
   adapter,
